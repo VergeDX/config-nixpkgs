381513c0dd725784a93ca7bdf85b1b5809e849bf
Vanilla <neko@hydev.org>
2021-06-09 09:44:35 +0800

    [+] nvidia-offload script from NixOS wiki.

--- home.nix
+++ home.nix
@@ -169,6 +169,15 @@
     pkgs.tdesktop
     (pkgs.makeAutostartItem { name = "telegramdesktop"; package = pkgs.tdesktop; })
 
+    # https://nixos.wiki/wiki/Nvidia
+    (pkgs.writeShellScriptBin "nvidia-offload" ''
+      export __NV_PRIME_RENDER_OFFLOAD=1
+      export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+      export __GLX_VENDOR_LIBRARY_NAME=nvidia
+      export __VK_LAYER_NV_optimus=NVIDIA_only
+      exec -a "$0" "$@"
+    '')
+
     pkgs.gimp-with-plugins
     pkgs.alacritty
     pkgs.remmina
