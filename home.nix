257be255353062b71d06bb53bf4f8859f7f0d23c
Vanilla <neko@hydev.org>
2021-06-09 09:17:46 +0800

    [+] Add 3 fish plugins: done, pisces, plugin-bang-bang.

--- home.nix
+++ home.nix
@@ -34,6 +34,32 @@
         eval powerline-go -error $status -jobs (jobs -p | wc -l)
     end
   '';
+  programs.fish.plugins = [
+    {
+      name = "done";
+      src = pkgs.fetchgit {
+        url = "https://github.com/franciscolourenco/done";
+        rev = "1.16.2";
+        sha256 = "sha256-W3Xw5878ixzEaFCtxCqfDu5kxOYUwdENdC+cBvwAwSE=";
+      };
+    }
+    {
+      name = "pisces";
+      src = pkgs.fetchgit {
+        url = "https://github.com/laughedelic/pisces";
+        rev = "v0.7.0";
+        sha256 = "sha256-Oou2IeNNAqR00ZT3bss/DbhrJjGeMsn9dBBYhgdafBw=";
+      };
+    }
+    {
+      name = "plugin-bang-bang";
+      src = pkgs.fetchgit {
+        url = "https://github.com/oh-my-fish/plugin-bang-bang";
+        rev = "f969c618301163273d0a03d002614d9a81952c1e";
+        sha256 = "sha256-A8ydBX4LORk+nutjHurqNNWFmW6LIiBPQcxS3x4nbeQ=";
+      };
+    }
+  ];
 
   programs.zsh.enable = true;
   # https://github.com/nix-community/home-manager/issues/1338
