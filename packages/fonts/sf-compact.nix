e264a3fc986c9cc52afba6b23bf5bed3ec8c1443
Vanilla <neko@hydev.org>
2021-06-13 14:20:23 +0800

    [+] Add self packaged font sf-compact.

---
+++ packages/fonts/sf-compact.nix
@@ -0,0 +1,18 @@
+{ stdenv, fetchurl, p7zip }:
+
+stdenv.mkDerivation rec {
+  name = "sf-compact";
+  src = fetchurl {
+    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Compact.dmg";
+    sha256 = "sha256-6dn4J0/fDR8+vR64paBIePuRcmMgi3oJ4ieRJa2CJPU=";
+  };
+
+  buildInputs = [ p7zip ];
+  unpackPhase = "7z x ${src}";
+  installPhase = ''
+    cd ./SanFranciscoCompact
+    7z x 'San Francisco Compact.pkg' && 7z x ./Payload~
+    mkdir -p $out/share/fonts/opentype/
+    cp -r ./Library/Fonts/ $out/share/fonts/opentype/SanFranciscoCompact
+  '';
+}
