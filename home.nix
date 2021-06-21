28a3f58df68115148159d9c7b3779404f298067c
Vanilla <neko@hydev.org>
2021-06-21 09:10:24 +0800

    [M] gnome pkgs -> ./pkgs/gnome.nix.

--- home.nix
+++ home.nix
@@ -49,38 +49,6 @@
     pkgs.guake
     (pkgs.makeAutostartItem { name = "guake"; package = pkgs.guake; })
 
-    pkgs.gnome.gnome-tweak-tool
-    (pkgs.callPackage ./packages/themes/layan-gtk-theme.nix { })
-    pkgs.breeze-gtk
-    pkgs.tela-icon-theme
-    (pkgs.callPackage ./packages/themes/flat-remix-gnome.nix { })
-    pkgs.gnomeExtensions.unite
-    pkgs.gnomeExtensions.runcat
-    pkgs.gnomeExtensions.jiggle
-    pkgs.gnomeExtensions.espresso
-    pkgs.gnomeExtensions.blur-me
-    pkgs.gnomeExtensions.openweather
-    pkgs.gnomeExtensions.tray-icons
-    pkgs.gnomeExtensions.simple-net-speed
-    pkgs.gnomeExtensions.proxy-switcher
-    pkgs.gnomeExtensions.extension-list
-    pkgs.gnomeExtensions.eclipse
-    pkgs.gnomeExtensions.screenshot-tool
-    # Credit: @Cyunrei
-    pkgs.gnomeExtensions.bluetooth-quick-connect
-    pkgs.gnomeExtensions.extensions-in-system-menu
-    (pkgs.callPackage ./packages/gnome/dash-to-dock-gnome40.nix { })
-    # https://www.linuxlinks.com/excellent-gnome-desktop-extensions/
-    pkgs.gnomeExtensions.arcmenu
-    pkgs.gnomeExtensions.gsconnect
-    # https://extensions.gnome.org/
-    pkgs.gnomeExtensions.sound-output-device-chooser
-    pkgs.gnomeExtensions.status-area-horizontal-spacing
-    pkgs.gnomeExtensions.window-is-ready-notification-remover
-    (pkgs.callPackage ./packages/gnome/lockkeys.nix { })
-    pkgs.gnomeExtensions.disconnect-wifi
-    pkgs.conky
-
     pkgs.arion
     pkgs.apfs-fuse
     # https://github.com/HeQuanX/navicat-keygen-tools/blob/main/README.zh-CN.md
@@ -268,5 +236,6 @@
 
     ./pkgs/ctf.nix
     ./pkgs/fonts.nix
+    ./pkgs/gnome.nix
   ];
 }
