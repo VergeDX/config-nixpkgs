05dffb7727d3bff4893c57aa80c0a236aef989bb
Vanilla <neko@hydev.org>
2021-06-22 21:45:07 +0800

    [U] Update dash-to-dock-gnome40 to latest commit 58e1cd0.

--- packages/gnome/dash-to-dock-gnome40.nix
+++ packages/gnome/dash-to-dock-gnome40.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchgit, glib, sassc }:
 stdenv.mkDerivation rec {
   name = "dash-to-dock-gnome40";
-  version = "a2d40e2";
+  version = "58e1cd0";
 
   buildInputs = [ glib sassc ];
   src = fetchgit {
     url = "https://github.com/ewlsh/dash-to-dock";
-    rev = "${version}a06117617bcbc5f85608c895c7734604e";
-    sha256 = "sha256-Kb4TlZAjXu7M/xL9maTZgJT19+PCopB5U9ZRXeaN/mM=";
+    rev = "${version}e7b8d82a95e7c892c4efbeb2f7b2632f2";
+    sha256 = "sha256-GnZaLQhm2chzygeodi4lpSh0VOIvZybIpdU9FGxGGFQ=";
   };
 
   installPhase = ''
