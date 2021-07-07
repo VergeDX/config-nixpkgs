08c657954a94c1be30bcfd94a4cc895db7281016
Vanilla <neko@hydev.org>
2021-07-07 15:40:13 +0800

    [M] Using compile way to pack gpick, ready to PR nixpkgs.

--- packages/gnome/gpick.nix
+++ packages/gnome/gpick.nix
@@ -1,24 +1,24 @@
-{ stdenv, fetchurl, dpkg, autoPatchelfHook, wrapGAppsHook, pkgs, ... }:
+{ stdenv, fetchgit, cmake, glib, boost, pkg-config, gtk3, ragel, lua, lib }:
+
 stdenv.mkDerivation rec {
   name = "gpick";
-  version = "0.2.5+git20161221-1build1";
+  version = "0.2.6";
 
-  src = fetchurl {
-    url = "http://mirrors.bfsu.edu.cn/ubuntu/pool/universe/g/${name}/${name}_${version}_amd64.deb";
-    sha256 = "sha256-eFIkoj0eeDlyV861uD2s2LwaKbp0E4EgXAzveFsvEX4=";
+  src = fetchgit {
+    url = "https://github.com/thezbyg/${name}";
+    rev = "${name}-${version}";
+    sha256 = "sha256-Z67EJRtKJZLoTUtdMttVTLkzTV2F5rKZ96vaothLiFo=";
   };
 
-  unpackCmd = "dpkg-deb -x ${src} .";
-  installPhase = "mkdir -p $out && cp -r * $out";
-  nativeBuildInputs = [
-    dpkg
-    autoPatchelfHook
-    wrapGAppsHook
-  ];
+  nativeBuildInputs = [ cmake ];
+  NIX_CFLAGS_COMPILE = "-I${glib.dev}/include/gio-unix-2.0";
+  buildInputs = [ boost pkg-config gtk3 ragel lua ];
 
-  buildInputs = [
-    pkgs.gtk2-x11
-    pkgs.boost165
-    (pkgs.callPackage ./liblua5.2-0.nix { })
-  ];
+  meta = with lib; {
+    description = "Advanced color picker written in C++ using GTK+ toolkit";
+    homepage = "http://www.gpick.org/";
+    license = licenses.bsd3;
+    maintainers = [ maintainers.vanilla ];
+    platforms = platforms.linux;
+  };
 }
