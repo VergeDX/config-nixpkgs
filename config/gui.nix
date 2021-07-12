77ed3b5be822e7823a035d9062a0b9b5d7325d95
Vanilla <neko@hydev.org>
2021-07-12 16:39:30 +0800

    [+] Adding vscode extensions, manage settings.json by nix.

--- config/gui.nix
+++ config/gui.nix
@@ -18,7 +18,6 @@
     (pkgs.makeAutostartItem { name = "albert"; package = pkgs.albert; })
     (pkgs.callPackage ../packages/gui/edex-ui.nix { })
 
-    pkgs.vscode
     pkgs.libreoffice
     (pkgs.wpsoffice.overrideAttrs (old: {
       version = "11.1.0.10161";
@@ -34,4 +33,36 @@
 
     (pkgs.callPackage ../packages/gui/google-earth-pro.nix { })
   ];
+
+  programs.vscode.enable = true;
+  programs.vscode.extensions = [
+    (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+      mktplcRef = {
+        publisher = "github";
+        name = "copilot";
+        version = "1.1.1959";
+        sha256 = "sha256-DuYJb1lzqITEpitEFyyXAs7H5NB9o3yXhSBV3CQ0YsI=";
+      };
+    })
+    (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+      mktplcRef = {
+        publisher = "unthrottled";
+        name = "doki-theme";
+        version = "14.0.0";
+        sha256 = "sha256-UL/j15dY5Y6FI/2Dr4HG4rAE/BXv3kDHuOc++UATL14=";
+      };
+    })
+  ];
+
+  programs.vscode.userSettings = {
+    "editor.inlineSuggest.enabled" = true;
+    "github.copilot.autocomplete.enable" = true;
+    "github.copilot.enable" = {
+      "*" = true;
+      "yaml" = false;
+      "plaintext" = true;
+      "markdown" = false;
+    };
+    "workbench.colorTheme" = "e828aaae-aa8c-4084-8993-d64697146930";
+  };
 }
