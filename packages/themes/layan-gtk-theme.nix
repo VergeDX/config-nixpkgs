bd61679d3c2ea383a23563fc95551e0b7ff82816
Vanilla <neko@hydev.org>
2021-07-13 10:37:48 +0800

    [+] More details for self-packaged layan-gtk-theme.nix.

--- packages/themes/layan-gtk-theme.nix
+++ packages/themes/layan-gtk-theme.nix
@@ -1,19 +1,30 @@
-{ stdenv, fetchgit }:
+{ stdenv
+, fetchFromGitHub
+, lib
+}:
+
 stdenv.mkDerivation rec {
   pname = "layan-gtk-theme";
   version = "2021-06-30";
 
-  src = fetchgit {
-    url = "https://github.com/vinceliuice/Layan-gtk-theme";
+  src = fetchFromGitHub {
+    owner = "vinceliuice";
+    repo = pname;
     rev = version;
     sha256 = "sha256-FI8+AJlcPHGOzxN6HUKLtPGLe8JTfTQ9Az9NsvVUK7g=";
   };
 
-  # https://github.com/NixOS/nixpkgs/blob/nixos-21.05/pkgs/data/themes/vimix/default.nix#L26
   installPhase = ''
     patchShebangs .
     mkdir -p $out/share/themes
-    name= ./install.sh -d $out/share/themes
-    rm $out/share/themes/*/{AUTHORS,LICENSE}
+    ./install.sh -d $out/share/themes -n Layan
   '';
+
+  meta = with lib; {
+    description = "A flat Material Design theme for GTK 3, GTK 2 and Gnome-Shell.";
+    homepage = "https://github.com/vinceliuice/Layan-gtk-theme";
+    license = licenses.gpl3;
+    platforms = platforms.linux;
+    maintainers = [ maintainers.vanilla ];
+  };
 }
