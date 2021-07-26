a840b15f99a499dd35425931653443e096c049f8
Vanilla <neko@hydev.org>
2021-07-26 21:23:30 +0000

    [-] Remove config.nix (NUR for nix repl).

--- config.nix
+++
@@ -1,8 +0,0 @@
-{
-  packageOverrides = pkgs: {
-    # https://github.com/nix-community/NUR#installation
-    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-      inherit pkgs;
-    };
-  };
-}
