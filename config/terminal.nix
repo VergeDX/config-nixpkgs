9a9195c98d7317f68c6e5f3af309823b2c478d7d
Vanilla <neko@hydev.org>
2021-08-03 08:26:56 +0800

    [+] Adding mcfly to query command history.

--- config/terminal.nix
+++ config/terminal.nix
@@ -93,4 +93,9 @@
     url = "https://raw.githubusercontent.com/dexpota/kitty-themes/master/themes/Solarized_Light.conf";
     sha256 = "sha256-1UN/jP63IqA0L3848pAjoeeG/uyoVD91zBexXzD3tvU=";
   };
+
+  # https://github.com/cantino/mcfly
+  programs.mcfly.enable = true;
+  programs.mcfly.enableFishIntegration = true;
+  programs.mcfly.enableLightTheme = true;
 }
