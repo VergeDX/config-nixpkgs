a559cbc45b3d03b8b3c5dbb0f3d1f47b12b6e53a
Vanilla <neko@hydev.org>
2021-08-20 16:09:22 +0800

    [+] Add a bunch of vim plugins, by follow NvChad/NvChad project.

--- config/neovim.nix
+++ config/neovim.nix
@@ -1,16 +1,42 @@
-{ programs, pkgs, ... }:
+{ programs, pkgs, home, ... }:
 {
   programs.neovim.enable = true;
   programs.neovim = {
     # https://github.com/NixOS/nixpkgs/blob/nixos-21.05/nixos/modules/programs/neovim.nix#L66
     plugins = [
       pkgs.vimPlugins.doki-theme-vim
+
+      # https://github.com/vim-airline/vim-airline#themes
       pkgs.vimPlugins.vim-airline
       pkgs.vimPlugins.vim-airline-themes
+
       pkgs.vimPlugins.vim-polyglot
       pkgs.vimPlugins.vim-lastplace
-      pkgs.vimPlugins.tabnine-vim
+      # pkgs.vimPlugins.tabnine-vim
       pkgs.vimPlugins.editorconfig-vim
+
+      pkgs.vimPlugins.coc-nvim
+      pkgs.vimPlugins.coc-tabnine
+
+      pkgs.vimPlugins.nvim-lspconfig
+      # https://github.com/NvChad/NvChad
+      # pkgs.vimPlugins.lspkind-nvim
+      # pkgs.vimPlugins.nvim-tree-lua
+
+      # https://github.com/nvim-telescope/telescope.nvim#installation
+      pkgs.vimPlugins.plenary-nvim
+      pkgs.vimPlugins.telescope-nvim
+
+      pkgs.vimPlugins.indent-blankline-nvim
+
+      # https://github.com/glepnir/galaxyline.nvim#install
+      # pkgs.vimPlugins.galaxyline-nvim
+      # pkgs.vimPlugins.nvim-web-devicons
+      # pkgs.vimPlugins.vim-devicons
+
+      # https://github.com/akinsho/nvim-bufferline.lua#installation
+      pkgs.vimPlugins.nvim-bufferline-lua
+      pkgs.vimPlugins.nvim-web-devicons
     ];
 
     # https://github.com/doki-theme/doki-theme-vim#installation
@@ -21,6 +47,15 @@
       syntax enable
       set number
       set showtabline=2
+
+      " https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rnix
+      lua require'lspconfig'.rnix.setup{}
+
+      " lua vim.opt.termguicolors = true
+      lua require("bufferline").setup{}
     '';
   };
+
+  # https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rnix
+  home.packages = [ pkgs.rnix-lsp ];
 }
