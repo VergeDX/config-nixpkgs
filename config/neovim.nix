7f85e98da743e8edb35ccc4690b5d78d744f71c6
Vanilla <neko@hydev.org>
2021-08-22 17:36:27 +0800

    [-] Start optimize vim startup, remove useless plugins.

--- config/neovim.nix
+++ config/neovim.nix
@@ -1,49 +1,28 @@
 { programs, pkgs, home, ... }:
 {
   programs.neovim.enable = true;
+  programs.neovim.coc.enable = true;
   programs.neovim = {
     # https://github.com/NixOS/nixpkgs/blob/nixos-21.05/nixos/modules/programs/neovim.nix#L66
     plugins = [
       pkgs.vimPlugins.doki-theme-vim
-
-      # https://github.com/vim-airline/vim-airline#themes
       pkgs.vimPlugins.vim-airline
+      pkgs.vimPlugins.vim-airline-clock
       pkgs.vimPlugins.vim-airline-themes
 
       pkgs.vimPlugins.vim-polyglot
       pkgs.vimPlugins.vim-lastplace
+      # Official's is a fork of ycm-core/YouCompleteMe.
       # pkgs.vimPlugins.tabnine-vim
       pkgs.vimPlugins.editorconfig-vim
 
-      pkgs.vimPlugins.coc-nvim
+      # Use module from home-manager instead.
+      # pkgs.vimPlugins.coc-nvim
       pkgs.vimPlugins.coc-tabnine
-
       pkgs.vimPlugins.nvim-lspconfig
-      # https://github.com/NvChad/NvChad
-      # pkgs.vimPlugins.lspkind-nvim
-
-      # https://github.com/kyazdani42/nvim-tree.lua#install
-      pkgs.vimPlugins.nvim-web-devicons
-      pkgs.vimPlugins.nvim-tree-lua
-
-      # https://github.com/nvim-telescope/telescope.nvim#installation
-      pkgs.vimPlugins.plenary-nvim
-      pkgs.vimPlugins.telescope-nvim
-
-      pkgs.vimPlugins.indent-blankline-nvim
-
-      # https://github.com/glepnir/galaxyline.nvim#install
-      # pkgs.vimPlugins.galaxyline-nvim
-      # pkgs.vimPlugins.nvim-web-devicons
-      # pkgs.vimPlugins.vim-devicons
-
-      # https://github.com/akinsho/nvim-bufferline.lua#installation
-      pkgs.vimPlugins.nvim-bufferline-lua
-      pkgs.vimPlugins.nvim-web-devicons
     ];
 
     # https://github.com/doki-theme/doki-theme-vim#installation
-    # https://github.com/doki-theme/doki-theme-vim/tree/master/colors
     # https://medium.com/@hql287/10-vim-tips-to-ease-the-learning-curve-c8234cbdafa5
     # https://vimjc.com/vim-powerline.html
     extraConfig = ''
@@ -53,9 +32,6 @@
 
       " https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rnix
       lua require'lspconfig'.rnix.setup{}
-
-      " lua vim.opt.termguicolors = true
-      lua require("bufferline").setup{}
     '';
   };
 
