61f19ca05f5098db549b53b66530e88f0a36582f
Vanilla <neko@hydev.org>
2021-09-09 08:11:18 +0800

    [-] config/develop.nix: Remove flutter.

--- config/develop.nix
+++ config/develop.nix
@@ -2,19 +2,9 @@
 let home_dir = "/home/vanilla";
 in
 {
-  # https://nixos.org/manual/nix/unstable/expressions/builtins.html
-  home.file.".config/flutter/settings".text = builtins.toJSON {
-    "enable-linux-desktop" = true;
-    "android-sdk" = "${pkgs.androidenv.androidPkgs_9_0.platform-tools}libexec/";
-  };
-
   home.sessionVariables = {
     PKG_CONFIG_PATH = "${home_dir}/.nix-profile/lib/pkgconfig:${home_dir}/.nix-profile/share/pkgconfig";
     CHROME_EXECUTABLE = "${pkgs.google-chrome}/bin/google-chrome-stable";
-
-    # https://mirrors.bfsu.edu.cn/help/flutter/
-    FLUTTER_STORAGE_BASE_URL = "https://mirrors.bfsu.edu.cn/flutter";
-    PUB_HOSTED_URL = "https://mirrors.bfsu.edu.cn/dart-pub";
   };
 
   home.packages = [
@@ -25,12 +15,9 @@ in
     pkgs.dex2jar
     pkgs.jd-gui
 
-    pkgs.dart
-    (pkgs.callPackage ../packages/override/flutter-override.nix { })
     pkgs.clang
     pkgs.cmake
     pkgs.ninja
-    pkgs.pkg-config
 
     pkgs.jetbrains-mono
 
