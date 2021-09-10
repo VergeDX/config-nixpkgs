4139026f867bd13c461ca4586bcd67b32f638f90
Vanilla <neko@hydev.org>
2021-09-10 09:09:08 +0800

    [+] packages/cutefish: Add cutefish-calculator.

---
+++ packages/cutefish/cutefish-calculator.nix
@@ -0,0 +1,27 @@
+let
+  pkgs = import <nixpkgs> { };
+  cutefish-calculator = pkgs.stdenv.mkDerivation rec {
+    name = "cutefish-calculator";
+    version = "0.4";
+
+    src = pkgs.fetchgit {
+      url = "https://github.com/cutefishos/calculator";
+      rev = version;
+      sha256 = "sha256-DMsRVI9QGDYLoJW99FofQHAztnIEVyJ2PT4xBOq5rbg=";
+    };
+
+    nativeBuildInputs = [ pkgs.cmake pkgs.libsForQt5.wrapQtAppsHook ];
+    buildInputs = [
+      pkgs.pkgs.libsForQt5.qt5.qtquickcontrols2
+      pkgs.libsForQt5.qt5.qttools
+      (import ./fishui.nix)
+      pkgs.libsForQt5.qt5.qtgraphicaleffects
+    ];
+
+    installPhase = ''
+      sed -i 's@/var/empty@'"$out"'@g' cmake_install.cmake
+      make install
+    '';
+  };
+in
+cutefish-calculator
