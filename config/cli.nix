a64eec902f5893ed8d66065560eceaa7e45135c0
Vanilla <neko@hydev.org>
2021-09-12 20:56:04 +0800

    [-] config/cli.nix: Remove nvidia-offload, install it as global.

--- config/cli.nix
+++ config/cli.nix
@@ -1,15 +1,6 @@
 { home, pkgs, ... }:
 {
   home.packages = [
-    # https://nixos.wiki/wiki/Nvidia
-    (pkgs.writeShellScriptBin "nvidia-offload" ''
-      export __NV_PRIME_RENDER_OFFLOAD=1
-      export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-      export __GLX_VENDOR_LIBRARY_NAME=nvidia
-      export __VK_LAYER_NV_optimus=NVIDIA_only
-      exec -a "$0" "$@"
-    '')
-
     pkgs.unzip
     pkgs.scrcpy
     pkgs.glxinfo
