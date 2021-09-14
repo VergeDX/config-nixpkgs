2f0b69fd000a464b5ca899ff8bd0f4b40b7e1e2d
Vanilla <neko@hydev.org>
2021-09-14 21:54:37 +0800

    [+] config/shells.nix: Enable Autosuggestions & SyntaxHighlighting.

--- config/shells.nix
+++ config/shells.nix
@@ -72,10 +72,11 @@
 
   # https://github.com/nix-community/home-manager/issues/1338
   programs.zsh.enable = true;
+  programs.zsh.enableAutosuggestions = true;
+  programs.zsh.enableSyntaxHighlighting = true;
   programs.zsh.initExtra = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
   # https://github.com/nix-community/home-manager/blob/master/modules/home-environment.nix#L230
   home.sessionVariables = { POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD = true; };
-  programs.zsh.enableSyntaxHighlighting = true;
 
   # https://github.com/wting/autojump
   programs.autojump.enable = true;
