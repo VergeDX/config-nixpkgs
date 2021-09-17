1b63cea0a7f59ab903cda9f90a55e9f57114e685
Vanilla <neko@hydev.org>
2021-09-17 17:16:45 +0800

    [M] config/terminal.nix: Alacritty theme -> Terminal.app.

--- config/terminal.nix
+++ config/terminal.nix
@@ -18,40 +18,29 @@
 
   # https://github.com/alacritty/alacritty/wiki/Color-schemes
   home.file.".config/alacritty/alacritty.yml".text = ''
-    # https://github.com/alacritty/alacritty/wiki/Color-schemes
-    # Colors (Solarized Light)
+    # Colors (Terminal.app Basic)
     colors:
-      # Default colors
       primary:
-        background: '#fdf6e3' # base3
-        foreground: '#657b83' # base00
-
-      # Cursor colors
-      cursor:
-        text:   '#fdf6e3' # base3
-        cursor: '#657b83' # base00
-
-      # Normal colors
+        background: '#FFFFFF'
+        foreground: '#000000'
       normal:
-        black:   '#073642' # base02
-        red:     '#dc322f' # red
-        green:   '#859900' # green
-        yellow:  '#b58900' # yellow
-        blue:    '#268bd2' # blue
-        magenta: '#d33682' # magenta
-        cyan:    '#2aa198' # cyan
-        white:   '#eee8d5' # base2
-
-      # Bright colors
+        black:      '#000000'
+        red:        '#990000'
+        green:      '#00A600'
+        yellow:     '#999900'
+        blue:       '#0000B2'
+        magenta:    '#B200B2'
+        cyan:       '#00A6B2'
+        white:      '#BFBFBF'
       bright:
-        black:   '#002b36' # base03
-        red:     '#cb4b16' # orange
-        green:   '#586e75' # base01
-        yellow:  '#657b83' # base00
-        blue:    '#839496' # base0
-        magenta: '#6c71c4' # violet
-        cyan:    '#93a1a1' # base1
-        white:   '#fdf6e3' # base3
+        black:      '#666666'
+        red:        '#E50000'
+        green:      '#00D900'
+        yellow:     '#E5E500'
+        blue:       '#0000FF'
+        magenta:    '#E500E5'
+        cyan:       '#00E5E5'
+        white:      '#E5E5E5'
 
     # https://wiki.gentoo.org/wiki/Alacritty#Font_configuration
     # Font configuration (changes require restart)
