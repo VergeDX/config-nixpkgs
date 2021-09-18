47b1a3661430ebda9b696aeefa8baeb624a86dfb
Vanilla <neko@hydev.org>
2021-09-18 15:06:50 +0800

    [+] config/browser.nix: Add systemd session variable, enable sync of chromium.

--- config/browser.nix
+++ config/browser.nix
@@ -9,4 +9,11 @@
   ];
 
   home.packages = [ pkgs.google-chrome ];
+
+  # https://wiki.archlinux.org/title/Chromium_(%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87)
+  # https://gist.github.com/foutrelis/14e339596b89813aa9c37fd1b4e5d9d5
+  systemd.user.sessionVariables = {
+    GOOGLE_DEFAULT_CLIENT_ID = "77185425430.apps.googleusercontent.com";
+    GOOGLE_DEFAULT_CLIENT_SECRET = "OTJgUOQcT7lO7GsGZq2G4IlT";
+  };
 }
