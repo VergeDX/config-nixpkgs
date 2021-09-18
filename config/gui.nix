5989ce0ef042b00ef0a887b51bcf58b57059fe79
Vanilla <neko@hydev.org>
2021-09-18 18:45:20 +0800

    [M] config/gui.nix: wpsoffice is fixed.

--- config/gui.nix
+++ config/gui.nix
@@ -16,17 +16,7 @@
     (pkgs.makeAutostartItem { name = "albert"; package = pkgs.albert; })
 
     pkgs.libreoffice
-    (pkgs.wpsoffice.overrideAttrs (old: {
-      version = "11.1.0.10161";
-      src = pkgs.fetchurl {
-        url = "https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/10161/wps-office_11.1.0.10161.XA_amd64.deb";
-        sha256 = "sha256-jKYIRChpPIGDP2/fE0QqnqkFJjjVvcDk11EgdfQAB8M";
-      };
-      postFixup = old.postFixup + ''
-        cd $out/share/applications/
-        sed -i 's/Exec=/Exec=steam-run /g' *.desktop
-      '';
-    }))
+    pkgs.wpsoffice
 
     pkgs.wireshark
 
