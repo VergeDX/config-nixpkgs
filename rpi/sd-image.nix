da03f2b7f18723468048a2ba2a4479fef5c8e492
Vanilla <neko@hydev.org>
2021-09-19 22:21:09 +0800

    [M] Move deviceTree patch to a new file.

--- rpi/sd-image.nix
+++ rpi/sd-image.nix
@@ -3,28 +3,7 @@
   imports = [
     # https://nixos.wiki/wiki/NixOS_on_ARM#Build_your_own_image
     <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix>
-  ];
-
-  # https://github.com/NixOS/nixpkgs/issues/135828
-  hardware.deviceTree.overlays = [
-    {
-      # https://github.com/NixOS/nixpkgs/issues/135828#issuecomment-918359063
-      name = "issuecomment-918359063";
-      dtsText = ''
-        // SPDX-License-Identifier: GPL-2.0
-        /dts-v1/;
-        /plugin/;
-        / {
-            compatible = "brcm,bcm2711";
-            fragment@1 {
-                target = <&emmc2bus>;
-                __overlay__ {
-                    dma-ranges = <0x00 0x00 0x00 0x00 0xfc000000>;
-                };
-            };
-        };
-      '';
-    }
+    ./issuecomment-918359063.nix
   ];
 
   ## Build
