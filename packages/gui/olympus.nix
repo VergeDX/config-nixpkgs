29f2f532e9bef1136ee59cc852cc6cd7e3e87710
Vanilla <neko@hydev.org>
2021-09-19 09:17:01 +0800

    [U] packages/gui/olympus.nix: 2316 -> 2369.

--- packages/gui/olympus.nix
+++ packages/gui/olympus.nix
@@ -2,12 +2,12 @@
 let
   olympus = pkgs.stdenv.mkDerivation rec {
     pname = "olympus";
-    version = "2316";
+    version = "2369";
 
     # https://everestapi.github.io/
     src = pkgs.fetchzip {
       url = "https://dev.azure.com/EverestAPI/Olympus/_apis/build/builds/${version}/artifacts?artifactName=linux.main&$format=zip#linux.main.zip";
-      sha256 = "sha256-WITw1KmfvmwdOc8MnZghCU+tNzsLbsi5KRzQ2u3ejmw=";
+      sha256 = "sha256-s4sk+b3vPB+Lfek1wh7PT/4bOaGt0wd8KDj8R5iCrDQ=";
     };
 
     buildInputs = [ pkgs.unzip ];
