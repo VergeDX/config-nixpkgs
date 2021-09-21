440312ca7fa847b02ad702e18ff87982c3434e4a
Vanilla <neko@hydev.org>
2021-09-21 15:36:39 +0800

    [+] rpi/sd-image.nix: Config for wlan0 networkd.

--- rpi/sd-image.nix
+++ rpi/sd-image.nix
@@ -10,4 +10,9 @@
   # https://nixos.wiki/wiki/Wpa_supplicant
   networking.wireless.enable = true;
   networking.wireless.networks = { "@Ruijie-s02C6".psk = "Jxustnc001"; };
+
+  # https://github.com/NickCao/flakes/blob/master/nixos/rpi/configuration.nix
+  networking = { useDHCP = false; useNetworkd = true; };
+  systemd.network.networks."wlan0".name = "wlan0";
+  systemd.network.networks."wlan0" = { enable = true; DHCP = "yes"; };
 }
