fbd11d41a01a0ca3660a1ac901bdec2bb4d66f5a
Vanilla <neko@hydev.org>
2021-09-22 21:58:47 +0800

    [F] Add missing wlan0 interfaces.

--- rpi/patch/issuecomment-924816587.nix
+++ rpi/patch/issuecomment-924816587.nix
@@ -1,5 +1,6 @@
 { ... }:
 {
   # https://unix.stackexchange.com/questions/398690
+  networking.wireless.interfaces = [ "wlan0" ];
   systemd.services."wpa_supplicant".serviceConfig = { "Type" = "dbus"; };
 }
