9795f30893d878cd284dca0ab44100f67b2431d0
Vanilla <neko@hydev.org>
2021-09-22 20:48:10 +0800

    [M] rpi/sd-image.nix: Move network config out.

--- rpi/sd-image.nix
+++ rpi/sd-image.nix
@@ -5,16 +5,9 @@
     <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix>
     # https://github.com/NixOS/nixpkgs/issues/135828
     ./issuecomment-918359063.nix
+
+    ./network.nix
   ];
-
-  # https://nixos.wiki/wiki/Wpa_supplicant
-  networking.wireless.enable = true;
-  networking.wireless.networks = { "@Ruijie-s02C6".psk = "Jxustnc001"; };
-
-  # https://github.com/NickCao/flakes/blob/master/nixos/rpi/configuration.nix
-  networking = { useDHCP = false; useNetworkd = true; };
-  systemd.network.networks."wlan0".name = "wlan0";
-  systemd.network.networks."wlan0" = { enable = true; DHCP = "yes"; };
 
   # https://unix.stackexchange.com/questions/398690
   systemd.services."wpa_supplicant".serviceConfig = { "Type" = "dbus"; };
