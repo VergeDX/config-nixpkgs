8024fc504f37e175ed50ea8c9472545ef3b65b58
Vanilla <neko@hydev.org>
2021-09-25 09:23:02 +0800

    [+] rpi/sd-image.nix: Do not compress to zstd.

--- rpi/sd-image.nix
+++ rpi/sd-image.nix
@@ -4,4 +4,7 @@
     <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix>
     ./configuration.nix
   ];
+
+  # https://github.com/NixOS/nixpkgs/blob/6845a74d4c85e0a392ee82a829d7fc8c9ecf4f08/nixos/modules/installer/sd-card/sd-image.nix#L137
+  sdImage.compressImage = false;
 }
