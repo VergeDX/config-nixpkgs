d77d8e431be4ec52e442f22fb8584380e6a5338e
Vanilla <neko@hydev.org>
2021-09-26 10:16:25 +0800

    [M] rpi/services-openssh.nix: Changes to ed25519 ssh key.

--- rpi/services-openssh.nix
+++ rpi/services-openssh.nix
@@ -2,9 +2,7 @@
 {
   services.openssh.enable = true;
   services.openssh.passwordAuthentication = false;
-  users.users.vanilla.openssh.authorizedKeys.keys = [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDNV2Q1yXZm0mx/SakkZDaqq1wZG9tpk/I09BoAFg51RyZyKiPAXixTz60fUlPmhnBKKauj/J9CSMd5TqeU466itN+A/87cT2rCiSPjMx9aQnJ3RANHi1QBgWnARyvR+/Dk5u0bV+ZVVjexmcapa7jA9i7hFIowqniQWeab7by8XWKebe68UWZ9eQ7A18iahCdHFvwl1uh+3TUjk+aaox7QWGMcqoZDqSB94PARncp95K2orHUouI373b/cnDonbzuCadMosGDeyEZPP4H7yat59GvVhnFbvVU3Yudbmd76G1EWaZkxMlKnGo8+ei/kbIaO0acc/4LrrTqJMFKqll3pFtyp59guJZz0jB2+NIJL99IjAhUwtpDsE+qZ4cE6OW5QJcOxS6YRhsXyIWuj8dqGwvv09GPjC/jskyKoC9b1vRsWwJECFg5bbOA/ObTv4QvQWjKUFtFUZVlDgrgQM6uhMxQwnyoV4j98roUG8Axnae/Aqj1JeeXAA7IRBL2qMdc= osu_Vanilla@126.com"
-  ];
+  users.users.vanilla.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINlS5xCmnWS2vMoP2ESJ4navm2CzhgbolMKhqJGxbwa/ osu_Vanilla@126.com" ];
 
   services.fail2ban.enable = true;
   services.fail2ban.bantime-increment.enable = true;
