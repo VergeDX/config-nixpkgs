eb62e654a39b51c8c07cb4c7c57f9057191542c2
Vanilla <neko@hydev.org>
2021-09-26 18:30:46 +0800

    [+] rpi/networking-wireless.nix: Sync proxy settings from NixOS-Laptop.

--- rpi/networking-wireless.nix
+++ rpi/networking-wireless.nix
@@ -17,4 +17,8 @@
   # https://nix.dev/tutorials/installing-nixos-on-a-raspberry-pi#installing-nixos
   networking.hostName = "NixOS-Raspi";
   networking.domain = "vanilla.local";
+
+  # Configure network proxy if necessary
+  networking.proxy.default = "http://NixOS-Laptop.local:7890";
+  networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 }
