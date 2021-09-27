7061261c6f9774e6dd6f4d69b68ce6aec6896cdf
Vanilla <neko@hydev.org>
2021-09-27 11:50:28 +0800

    [-] Remove all unused package .nix file.

--- config/fonts.nix
+++ config/fonts.nix
@@ -24,8 +24,6 @@
     pkgs.roboto-slab
     pkgs.roboto-mono
 
-    (pkgs.callPackage ../packages/resources/sf-pro.nix { })
-
     # https://wiki.archlinux.org/title/Font_Configuration_(%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87)/Chinese_(%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87)
     pkgs.dejavu_fonts
     pkgs.vdrsymbols
