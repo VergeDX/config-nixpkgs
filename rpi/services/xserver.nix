e510f31da4424e5d635914eca8fce62288611cbe
Vanilla <neko@hydev.org>
2021-09-27 12:01:25 +0800

    [+] rpi/services/xserver.nix: Install xmonad for xrdp, disable xserver.

--- rpi/services/xserver.nix
+++ rpi/services/xserver.nix
@@ -1,8 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # https://nixos.wiki/wiki/XMonad
-  services.xserver.enable = true;
-  services.xserver.windowManager.xmonad.enable = true;
+  # services.xserver.enable = true;
+  # services.xserver.windowManager.xmonad.enable = true;
+
+  environment.systemPackages = with pkgs; [ xmonad-with-packages xterm ];
 
   # https://nixos.wiki/wiki/Remote_Desktop
   services.xrdp.enable = true;
