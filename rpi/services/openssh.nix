efa903d1515ecb1ac0269c34dd8729314b3589eb
Vanilla <neko@hydev.org>
2021-09-27 10:39:29 +0800

    [+] rpi/services/openssh.nix: keys for users vanilla & root.

--- rpi/services/openssh.nix
+++ rpi/services/openssh.nix
@@ -1,8 +1,15 @@
 { ... }:
+let keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINlS5xCmnWS2vMoP2ESJ4navm2CzhgbolMKhqJGxbwa/ osu_Vanilla@126.com" ];
+in
 {
   services.openssh.enable = true;
   services.openssh.passwordAuthentication = false;
-  users.users.vanilla.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINlS5xCmnWS2vMoP2ESJ4navm2CzhgbolMKhqJGxbwa/ osu_Vanilla@126.com" ];
+
+  # https://github.com/serokell/deploy-rs/issues/25#issuecomment-740067529
+  users.users = {
+    "vanilla".openssh.authorizedKeys.keys = keys;
+    "root".openssh.authorizedKeys.keys = keys;
+  };
 
   services.fail2ban.enable = true;
   services.fail2ban.bantime-increment.enable = true;
