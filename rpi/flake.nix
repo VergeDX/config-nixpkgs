95bd54bba795a5c74f68d63fb9e30868d7da4bf5
Vanilla <neko@hydev.org>
2021-09-27 11:02:31 +0800

    [+] deploy-rs for NixOS-Raspi, awesome!

--- rpi/flake.nix
+++ rpi/flake.nix
@@ -4,21 +4,41 @@
   # https://github.com/nix-community/home-manager#nix-flakes
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   inputs.home-manager.url = "github:nix-community/home-manager";
+  # https://github.com/serokell/deploy-rs
+  inputs.deploy-rs.url = "github:serokell/deploy-rs";
 
   # https://nixos.wiki/wiki/Flakes#Using_nix_flakes_with_NixOS
-  outputs = { self, nixpkgs, nixos-hardware, home-manager }: {
-    nixosConfigurations."NixOS-Raspi" = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-      modules = [
-        ./configuration.nix
-        nixos-hardware.nixosModules.raspberry-pi-4
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users."vanilla" = import ./home-manager/home.nix;
-        }
-      ];
+  outputs = { self, nixpkgs, nixos-hardware, home-manager, deploy-rs }:
+    let rpi = {
+      hostName = "NixOS-Raspi";
+      arch = "aarch64-linux";
+      user = "vanilla";
+    }; in
+    rec {
+      nixosConfigurations."${rpi.hostName}" = nixpkgs.lib.nixosSystem {
+        system = "${rpi.arch}";
+        modules = [
+          ./configuration.nix
+          nixos-hardware.nixosModules.raspberry-pi-4
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users."${rpi.user}" = import ./home-manager/home.nix;
+          }
+        ];
+      };
+
+      deploy.nodes."${rpi.hostName}" = {
+        # https://github.com/NickCao/flakes/blob/d874d24e5718e34141647e1048cb489afa50808b/flake.nix#L109
+        hostname = "${rpi.hostName}.local";
+        # https://github.com/serokell/deploy-rs/issues/25#issuecomment-740067529
+        sshUser = "root";
+
+        profiles.system = {
+          user = "${rpi.user}";
+          path = deploy-rs.lib."${rpi.arch}".activate.nixos nixosConfigurations."${rpi.hostName}";
+        };
+      };
     };
-  };
 }
