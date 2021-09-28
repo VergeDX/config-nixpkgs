087b43330e571baf6303de4823c889f18c028994
Vanilla <neko@hydev.org>
2021-09-28 15:22:30 +0800

    [M] Move boot.* config to rpi/boot.nix.

---
+++ rpi/boot.nix
@@ -0,0 +1,9 @@
+{ pkgs, ... }:
+{
+  # https://github.com/NixOS/nixpkgs/issues/111683#issuecomment-771986512
+  # https://forums.xilinx.com/t5/Embedded-Linux/U-boot-v2019-1-ERROR-Did-not-find-a-cmdline-Flattened-Device/td-p/991415
+  boot.kernelPackages = pkgs.linuxPackages;
+
+  boot.loader.generic-extlinux-compatible.enable = true;
+  boot.loader.generic-extlinux-compatible.configurationLimit = 3;
+}
--- rpi/configuration.nix
+++ rpi/configuration.nix
@@ -12,18 +12,12 @@
     ./nix/flakes.nix
 
     ./users-immutable.nix
+    ./boot.nix
 
     ./services/openssh.nix
     ./services/xserver.nix
     ./services/avahi.nix
   ];
-
-  # https://github.com/NixOS/nixpkgs/issues/111683#issuecomment-771986512
-  # https://forums.xilinx.com/t5/Embedded-Linux/U-boot-v2019-1-ERROR-Did-not-find-a-cmdline-Flattened-Device/td-p/991415
-  boot.kernelPackages = pkgs.linuxPackages;
-
-  boot.loader.generic-extlinux-compatible.enable = true;
-  boot.loader.generic-extlinux-compatible.configurationLimit = 3;
 
   # https://nix.dev/tutorials/installing-nixos-on-a-raspberry-pi#installing-nixos
   fileSystems = {
