ed5bf8d59b219d59d061088e6534912dbb3e7fc5
Vanilla <osu_Vanilla@126.com>
2021-10-01 13:43:57 +0800

    [M] ./rpi/secrets/: Reinstall Raspi NixOS, agenix --rekey.

--- rpi/secrets/secrets.nix
+++ rpi/secrets/secrets.nix
@@ -1,9 +1,9 @@
 let
   vanilla-laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ//aL4yaW0iJychDr++qmE1/p73FkFpslQCUI3V00Tp osu_Vanilla@126.com";
-  vanilla-raspi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPOkOC5QHENPGhZlQcqPnXuaFvPY8N1EkhKBgAjIWOGe osu_Vanilla@126.com";
+  vanilla-raspi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILKPU5IvilHjCrjW0ppXXrjzslTiCMpSBlLZjB89Z7Rb root@NixOS-Raspi";
 
   laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHhc7Qz+qRW8HExnZfoCCErxbtDn/X0nSFDS6lPgmYX4 root@nixos";
-  raspi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDdByMiIWa9q/YW8G86q6jmb1xpNeKkv1QTrWHJf151Y root@NixOS-Raspi";
+  raspi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHMpE4EHEYbzMiyiOvfXG4FBgyz5AhLW46WsRDEl90Rv osu_Vanilla@126.com";
   all = [ vanilla-laptop laptop vanilla-raspi raspi ];
 in
 {
