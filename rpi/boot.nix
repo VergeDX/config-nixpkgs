51051228814b672eccc8580cfbce075942597931
Vanilla <osu_Vanilla@126.com>
2021-10-02 21:22:18 +0800

    [M] rpi/boot.nix: Linux zen kernel on raspi.

--- rpi/boot.nix
+++ rpi/boot.nix
@@ -2,7 +2,7 @@
 {
   # https://github.com/NixOS/nixpkgs/issues/111683#issuecomment-771986512
   # https://forums.xilinx.com/t5/Embedded-Linux/U-boot-v2019-1-ERROR-Did-not-find-a-cmdline-Flattened-Device/td-p/991415
-  boot.kernelPackages = pkgs.linuxPackages;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 
   boot.loader.generic-extlinux-compatible.enable = true;
   boot.loader.generic-extlinux-compatible.configurationLimit = 7;
