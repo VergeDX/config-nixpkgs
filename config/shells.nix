429ac32124a4500810efb0da655ca62752732508
Vanilla <osu_Vanilla@126.com>
2021-10-03 09:56:00 +0800

    [-] Revert starship -> silver.

--- config/shells.nix
+++ config/shells.nix
@@ -2,51 +2,29 @@
 {
   programs.fish.enable = true;
   # https://starship.rs/zh-CN/config/#%E6%8F%90%E7%A4%BA%E7%AC%A6
-  # programs.starship.enable = true;
-  # programs.starship.settings = {
-  #   add_newline = false;
-  #   line_break = { disabled = true; };
-  # };
+  programs.starship.enable = true;
+  programs.starship.settings = {
+    add_newline = false;
+    line_break = { disabled = true; };
+  };
 
   # https://github.com/nvbn/thefuck/wiki/Shell-aliases#fish
   programs.fish.shellInit = "${pkgs.thefuck}/bin/thefuck --alias | source";
   programs.fish.shellAliases = { top = "bpytop"; };
   home.packages = [
+    pkgs.powerline-go
+    pkgs.powerline-rs
+
     pkgs.starship
     pkgs.nur.repos.ilya-fedin.silver
+
+    pkgs.nushell
   ] ++ [
-    pkgs.meslo-lgs-nf
-    pkgs.nerdfonts
     pkgs.powerline-fonts
+    pkgs.powerline
+    pkgs.powerline-symbols
+    pkgs.meslo-lgs-nf
   ];
-
-  # https://github.com/reujab/silver/tree/v2.0.0#configuration
-  home.file.".config/silver/silver.toml".text = ''
-    [[left]]
-    name = "dir"
-    color.background = "blue"
-    color.foreground = "black"
-
-    [[left]]
-    name = "git"
-    color.background = "green"
-    color.foreground = "black"
-
-    [[right]]
-    name = "status"
-    color.background = "white"
-    color.foreground = "black"
-
-    [[right]]
-    name = "cmdtime"
-    color.background = "magenta"
-    color.foreground = "black"
-
-    [[right]]
-    name = "shell"
-    color.background = "green"
-    color.foreground = "black"
-  '';
 
   programs.fish.plugins = [
     {
@@ -87,14 +65,6 @@
         url = "https://github.com/kidonng/nix-completions.fish";
         rev = "dcb15c4f7d3e85d8f7cd3eb09b3014ccd278aab8";
         sha256 = "sha256-3HHcaX2yVZXrRjOeSlI8tPhSjRR2tPU6AWq6eU5nPOs=";
-      };
-    }
-    {
-      name = "silver";
-      src = pkgs.fetchgit {
-        url = "https://github.com/silver-prompt/fish";
-        rev = "c1a31f16a2739998241e181c8f453999321b4c67";
-        sha256 = "sha256-hzCn760N4+NZDjtLj+0ZoO0IgV61yOzRAHvj2MQOqd4=";
       };
     }
   ];
