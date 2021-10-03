b4495c3d3e15526cf5d52b5c6ef2ba7148274156
Vanilla <osu_Vanilla@126.com>
2021-10-03 18:51:21 +0800

    [+] config/terminal.nix: Rose Pine Dawn theme for terminal Alacritty.

--- config/terminal.nix
+++ config/terminal.nix
@@ -13,53 +13,38 @@
   ];
 
   # https://github.com/alacritty/alacritty/wiki/Color-schemes
-  home.file.".config/alacritty/alacritty.yml".text = ''
-    # Colors (Terminal.app Basic)
-    colors:
-      primary:
-        background: '#FFFFFF'
-        foreground: '#000000'
-      normal:
-        black:      '#000000'
-        red:        '#990000'
-        green:      '#00A600'
-        yellow:     '#999900'
-        blue:       '#0000B2'
-        magenta:    '#B200B2'
-        cyan:       '#00A6B2'
-        white:      '#BFBFBF'
-      bright:
-        black:      '#666666'
-        red:        '#E50000'
-        green:      '#00D900'
-        yellow:     '#E5E500'
-        blue:       '#0000FF'
-        magenta:    '#E500E5'
-        cyan:       '#00E5E5'
-        white:      '#E5E5E5'
+  home.file.".config/alacritty/alacritty.yml".text =
+    let rose-pine-dawn.yml = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/rose-pine/alacritty/main/rose-pine-dawn.yml";
+      sha256 = "sha256-/1y/sYIugR27QgbM2rG4WganTPbdqbAgnbrjkDXIgUg=";
+    }; in
+    ''
+      # https://github.com/rose-pine/alacritty#usage
+      import:
+        - ${rose-pine-dawn.yml}
 
-    # https://wiki.gentoo.org/wiki/Alacritty#Font_configuration
-    # Font configuration (changes require restart)
-    font:
-      # The normal (roman) font face to use.
-      normal:
-        family: Hack
-        # Style can be specified to pick a specific face.
-        style: Regular
+      # https://wiki.gentoo.org/wiki/Alacritty#Font_configuration
+      # Font configuration (changes require restart)
+      font:
+        # The normal (roman) font face to use.
+        normal:
+          family: Hack
+          # Style can be specified to pick a specific face.
+          style: Regular
 
-      # The bold font face
-      bold:
-        family: Hack
-        # Style can be specified to pick a specific face.
-        # style: Bold
+        # The bold font face
+        bold:
+          family: Hack
+          # Style can be specified to pick a specific face.
+          # style: Bold
 
-      # The italic font face
-      italic:
-        family: Hack
-        # Style can be specified to pick a specific face.
-        # style: Italic
-      size: 11.0
-  '';
+        # The italic font face
+        italic:
+          family: Hack
+          # Style can be specified to pick a specific face.
+          # style: Italic
+        size: 11.0
+    '';
 
   # https://sw.kovidgoyal.net/kitty/
   programs.kitty.enable = true;
