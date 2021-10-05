e155376276ccdd2b184323a26121744f2c14c9e7
Vanilla <osu_Vanilla@126.com>
2021-10-06 07:49:34 +0800

    [U] packages/cli/colorized-logs.nix: 2.5 -> 2.6.

--- packages/cli/colorized-logs.nix
+++ packages/cli/colorized-logs.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchFromGitHub, cmake, ... }:
 stdenv.mkDerivation rec {
   name = "colorized-logs";
-  version = "2.5";
+  version = "2.6";
 
   src = fetchFromGitHub {
     owner = "kilobyte";
     repo = name;
     rev = "v${version}";
-    sha256 = "sha256-5pfQrd2NbPdVh/R+w3rjQAMY0s7k4VkSljtlQbfyITc=";
+    sha256 = "sha256-QiZeIYeIWA3C7wYi2G2EItdW+jLjVrCbIYllur/RtY8=";
   };
 
   nativeBuildInputs = [ cmake ];
