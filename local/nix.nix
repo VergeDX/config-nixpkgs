a1f22427ac5d6bf005c765c2ea2a1a0a51543538
Vanilla <osu_Vanilla@126.com>
2021-10-05 11:13:44 +0800

    [M] local/nix.nix: Replace mirror bfsu -> sjtu.

--- local/nix.nix
+++ local/nix.nix
@@ -16,7 +16,7 @@
   # sudo nixos-rebuild switch --option substituters https://cache.nixos.org/
   nix.binaryCaches = [
     "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-    "https://mirrors.bfsu.edu.cn/nix-channels/store"
+    "https://mirror.sjtu.edu.cn/nix-channels/store"
     "https://cache.nixos.org/"
   ];
 }
