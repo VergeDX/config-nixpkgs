1fe388f73bae68177de953e95bc2a629d8309ea4
Vanilla <osu_Vanilla@126.com>
2021-10-07 10:06:23 +0800

    [M] config/develop.nix: Reformat code, use jb jdk for all jetbrains IDE.

--- config/develop.nix
+++ config/develop.nix
@@ -1,42 +1,16 @@
 { home, pkgs, ... }:
+let
+  useJbJdk = pkgsJb: pkgsJb.overrideAttrs (old: {
+    jdk = pkgs.jetbrains.jdk;
+  });
+in
 {
-  home.packages = [
-    pkgs.jetbrains.datagrip
-
-    pkgs.android-tools
-    pkgs.android-studio
-    pkgs.apktool
-    pkgs.dex2jar
-    pkgs.jd-gui
-
-    pkgs.clang
-    pkgs.cmake
-    pkgs.ninja
-
-    pkgs.jetbrains.idea-ultimate
-    pkgs.jdk11 # pkgs.jetbrains.jdk
-    pkgs.kotlin
-
-    pkgs.stack
-    pkgs.ghc
-    pkgs.haskellPackages.hlint
-    pkgs.haskellPackages.hoogle
-    pkgs.haskellPackages.ormolu
-    pkgs.haskellPackages.stylish-haskell
-
-    pkgs.jetbrains.pycharm-professional
-    pkgs.python3Full
-
-    pkgs.jetbrains.clion
-    pkgs.cmake
-    pkgs.gnumake
-
-    pkgs.maven
-    pkgs.gradle
-
-    pkgs.telnet
-    pkgs.binwalk
-  ] ++ (with pkgs; [ influxdb2 telegraf dasel ]);
+  home.packages = (builtins.map useJbJdk (with pkgs.jetbrains;
+    [ datagrip idea-ultimate pycharm-professional clion ]))
+  ++ (with pkgs; [ android-tools android-studio apktool dex2jar jd-gui ])
+  ++ (with pkgs; [ jdk11 kotlin maven gradle ]) # Java & Kotlin
+  ++ (with pkgs; [ python2Full python3Full ]) # Python 3
+  ++ (with pkgs; [ cmake gnumake clang cmake ninja ]); # C / C++
 
   # https://stackoverflow.com/questions/26523804/global-gradle-proxy-settings
   home.file.".gradle/gradle.properties".text = ''
