553c8e22b204fca106d3e4d00e91f6d917c22512
Vanilla <osu_Vanilla@126.com>
2021-10-08 15:44:58 +0800

    [+] packages/fonts/common.nix: Re-packaging all Apple Fonts.

--- config/fonts.nix
+++ config/fonts.nix
@@ -5,7 +5,35 @@ with pkgs; {
   home.packages = [ source-han-sans source-han-mono source-han-serif source-han-code-jp ]
     ++ [ noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji noto-fonts-emoji-blob-bin ]
     ++ [ roboto roboto-slab roboto-mono ]
-    ++ [ dejavu_fonts vdrsymbols hack-font vistafonts nerdfonts ];
+    ++ [ dejavu_fonts vdrsymbols hack-font vistafonts nerdfonts ]
+    ++ (with pkgs; [
+    # https://developer.apple.com/fonts/
+    (callPackage ../packages/fonts/common.nix {
+      name = "SF-Pro";
+      fontName = "San Francisco Pro";
+      sha256 = "sha256-Q2QgwnxNFf+zzLbZ/gF2uvE3TZpapmQTUY4enMmMCOM=";
+    })
+    (callPackage ../packages/fonts/common.nix {
+      name = "SF-Compact";
+      fontName = "San Francisco Compact";
+      sha256 = "sha256-1wEChhE6B3Cj6HvmvOfFP26/BHvKbwdVAVT9b8he0sI=";
+    })
+    (callPackage ../packages/fonts/common.nix {
+      name = "SF-Mono";
+      fontName = "SF Mono Fonts";
+      sha256 = "sha256-dsQV6a7gUkCHzGoEIRPbazjxoZ8uKG+AeqKXVkYTcKk=";
+    })
+    (callPackage ../packages/fonts/common.nix {
+      name = "SF-Arabic";
+      fontName = "SF Arabic Fonts";
+      sha256 = "sha256-DI76Da0L+J56z7OHGTudo76PWEFUYuE1i5lxK/xiB6o=";
+    })
+    (callPackage ../packages/fonts/common.nix {
+      name = "NY";
+      fontName = "NY Fonts";
+      sha256 = "sha256-qW/yCo4UPxz/MT7AXtK40pCjuf9gEKsGIZMldMn3lAI=";
+    })
+  ]);
 
   # https://wiki.archlinux.org/title/Localization_(%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87)/Simplified_Chinese_(%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87)
   # https://github.com/yrashk/nix-home/blob/master/home.nix#L285
