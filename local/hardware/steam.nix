aaa305cc1a535b3db0a2389bc4184a007f2e4b10
Vanilla <osu_Vanilla@126.com>
2021-10-09 12:58:58 +0800

    [-] local/hardware/steam.nix: Remove support of Steam Controller.

--- local/hardware/steam.nix
+++ local/hardware/steam.nix
@@ -5,8 +5,6 @@
 
   # https://github.com/NixOS/nixpkgs/issues/25957
   hardware.opengl.driSupport32Bit = true;
-  # https://nixos.wiki/wiki/Steam
-  hardware.steam-hardware.enable = true;
 
   # https://github.com/NixOS/nixpkgs/issues/101281
   hardware.xpadneo.enable = true;
