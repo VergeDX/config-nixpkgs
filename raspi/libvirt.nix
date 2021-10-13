a0da454a7fa03f99495693cce0805209b48ee863
Vanilla <osu_Vanilla@126.com>
2021-10-13 10:32:51 +0800

    [+] raspi/libvirt.nix: extraGroups += libvirtd.

--- raspi/libvirt.nix
+++ raspi/libvirt.nix
@@ -1,6 +1,9 @@
 { ... }:
 {
+  # https://nixos.wiki/wiki/Libvirt
   virtualisation.libvirtd.enable = true;
+  users.users."vanilla".extraGroups = [ "libvirtd" ];
+
   # https://nixos.wiki/wiki/Virt-manager
   programs.dconf.enable = true;
 }
