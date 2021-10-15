d3cbe5b3a7c1c5295ab83315898ed3976ea6e7b3
Vanilla <osu_Vanilla@126.com>
2021-10-15 16:38:09 +0800

    [+] raspi/services/xserver.nix: wallpaper, theme + icon + cursor.

--- raspi/services/xserver.nix
+++ raspi/services/xserver.nix
@@ -2,6 +2,20 @@
 {
   # https://nixos.wiki/wiki/Xorg
   services.xserver.enable = true;
+
+  # Default display manager of xmonad.
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.lightdm.background =
+    # https://github.com/NixOS/nixos-artwork/tree/master/wallpapers
+    pkgs.nixos-artwork.wallpapers."nineish".gnomeFilePath;
+
+  services.xserver.displayManager.lightdm.greeters.gtk.enable = true;
+  services.xserver.displayManager.lightdm.greeters.gtk = {
+    theme = { package = pkgs.layan-gtk-theme; name = "Layan-light-solid"; };
+    iconTheme = { package = pkgs.tela-icon-theme; name = "Tela-pink"; };
+    cursorTheme = { package = pkgs.libsForQt5.breeze-qt5; name = "breeze_cursors"; };
+  };
+
   # https://nixos.wiki/wiki/XMonad
   services.xserver.windowManager.xmonad.enable = true;
 
