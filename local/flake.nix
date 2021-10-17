bbcaa5627a49a410ce780a6104d7944947a6d88b
Vanilla <osu_Vanilla@126.com>
2021-10-17 19:06:52 +0800

    [M] ./local/flake.nix: Reformat code.

--- local/flake.nix
+++ local/flake.nix
@@ -5,9 +5,7 @@
     # https://nixos.wiki/wiki/Flakes#Using_nix_flakes_with_NixOS
     nixosConfigurations."NixOS-Laptop" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-      ];
+      modules = [ ./configuration.nix ];
     };
   };
 }
