b2a25fc3a26f7bfdbc6ab65a0041b99c3af4fe18
Vanilla <osu_Vanilla@126.com>
2021-10-19 19:46:27 +0800

    [+] local/nix/nixbuild.nix: Enable socks5 ProxyCommand.

--- local/nix/nixbuild.nix
+++ local/nix/nixbuild.nix
@@ -1,10 +1,12 @@
 { pkgs, ... }:
 # https://docs.nixbuild.net/getting-started/#quick-nixos-configuration
 {
+  # https://superuser.com/questions/454210/how-can-i-use-ssh-with-a-socks-5-proxy
   programs.ssh.extraConfig = ''
     Host eu.nixbuild.net
       PubkeyAcceptedKeyTypes ssh-ed25519
       IdentityFile /home/vanilla/.ssh/id_ed25519
+      ProxyCommand ${pkgs.netcat}/bin/nc -X 5 -x 127.0.0.1:1089 %h %p
   '';
 
   programs.ssh.knownHosts = {
