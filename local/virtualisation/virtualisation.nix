d98920ad87592b6fa461694fee88f62969642855
Vanilla <osu_Vanilla@126.com>
2021-10-20 10:19:54 +0800

    [+] local/virtualisation/virtualisation.nix: emulatedSystems += riscv64-linux.

--- local/virtualisation/virtualisation.nix
+++ local/virtualisation/virtualisation.nix
@@ -2,7 +2,7 @@
 {
   # https://nixos.wiki/wiki/Podman
   virtualisation.podman.enable = true;
-  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" "riscv64-linux" ];
 
   # https://docs.hercules-ci.com/arion/#_nixos
   environment.systemPackages = with pkgs; [ arion docker-client ];
