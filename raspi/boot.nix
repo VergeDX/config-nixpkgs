e53fa20f7be4f3b4d668a443e5a089651a94ce8c
Vanilla <osu_Vanilla@126.com>
2021-10-24 17:07:34 +0800

    [M] Move linux_rpi4.override out -> raspi/linux_rpi4.nix.

--- raspi/boot.nix
+++ raspi/boot.nix
@@ -1,19 +1,10 @@
 { pkgs, lib, ... }:
+let linux_rpi4 = pkgs.callPackage ./linux_rpi4.nix { };
+in
 {
   boot.initrd.availableKernelModules = lib.mkForce [ ];
   # https://nixos.wiki/wiki/Linux_kernel#Pinning_a_kernel_version
-  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_rpi4.override {
-    argsOverride = rec {
-      src = pkgs.fetchgit {
-        url = "https://github.com/raspberrypi/linux";
-        rev = "1.20211007";
-        sha256 = "sha256-vI0OMrJl+xc9KCQDrgjQB7FG2QsdKcY7WNwXTEJnzso=";
-      };
-
-      version = "5.10.y";
-      modDirVersion = "5.10.63";
-    };
-  });
+  boot.kernelPackages = pkgs.linuxPackagesFor linux_rpi4;
 
   boot.loader.generic-extlinux-compatible.enable = true;
   boot.loader.generic-extlinux-compatible.configurationLimit = 13;
