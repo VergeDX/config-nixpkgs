ced85ed0bc68373581357dc7867438921991b592
Vanilla <osu_Vanilla@126.com>
2021-10-24 13:07:11 +0800

    [+] raspi/boot.nix: Use latest raspi linux kernel.

--- raspi/boot.nix
+++ raspi/boot.nix
@@ -1,8 +1,19 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
-  # https://github.com/NixOS/nixpkgs/issues/111683#issuecomment-771986512
-  # https://forums.xilinx.com/t5/Embedded-Linux/U-boot-v2019-1-ERROR-Did-not-find-a-cmdline-Flattened-Device/td-p/991415
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.initrd.availableKernelModules = lib.mkForce [ ];
+  # https://nixos.wiki/wiki/Linux_kernel#Pinning_a_kernel_version
+  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_rpi4.override {
+    argsOverride = rec {
+      src = pkgs.fetchgit {
+        url = "https://github.com/raspberrypi/linux";
+        rev = "1.20211007";
+        sha256 = "sha256-vI0OMrJl+xc9KCQDrgjQB7FG2QsdKcY7WNwXTEJnzso=";
+      };
+
+      version = "5.10.y";
+      modDirVersion = "5.10.63";
+    };
+  });
 
   boot.loader.generic-extlinux-compatible.enable = true;
   boot.loader.generic-extlinux-compatible.configurationLimit = 13;
