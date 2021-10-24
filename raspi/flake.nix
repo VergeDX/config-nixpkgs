7b08f127aef6dae462191cc170c432e9989c0219
Vanilla <osu_Vanilla@126.com>
2021-10-24 17:08:57 +0800

    [M] home-manager/home.nix -> home.nix.

--- raspi/flake.nix
+++ raspi/flake.nix
@@ -32,7 +32,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users."${rpi.user}" = import ./home-manager/home.nix;
+            home-manager.users."${rpi.user}" = import ./home.nix;
           }
         ];
       };
