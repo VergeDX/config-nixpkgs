114191fa9881a5bf51ee38984d5ce22aa8e98615
Vanilla <osu_Vanilla@126.com>
2021-10-25 15:18:27 +0800

    [M] local/hardware/hardware.nix: Reformat code.

--- local/hardware/hardware.nix
+++ local/hardware/hardware.nix
@@ -20,7 +20,12 @@
   time.hardwareClockInLocalTime = true;
 
   # https://nixos.wiki/wiki/Accelerated_Video_Playback
-  nixpkgs.config.packageOverrides = pkgs: { vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; }; };
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override {
+      enableHybridCodec = true;
+    };
+  };
+
   hardware.opengl = {
     enable = true;
     extraPackages = with pkgs; [ intel-media-driver vaapiIntel vaapiVdpau libvdpau-va-gl ];
