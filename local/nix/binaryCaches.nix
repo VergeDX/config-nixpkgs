1a671889d9afdb22b0618175ffce3c1e2a98901f
Vanilla <osu_Vanilla@126.com>
2021-10-28 16:33:23 +0800

    [+] local/nix/binaryCaches.nix: Add nix-community.

--- local/nix/binaryCaches.nix
+++ local/nix/binaryCaches.nix
@@ -4,6 +4,7 @@
   nix.binaryCaches = [
     "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
     "https://mirror.sjtu.edu.cn/nix-channels/store"
+    "https://nix-community.cachix.org"
     "https://cache.nixos.org/"
   ];
 }
