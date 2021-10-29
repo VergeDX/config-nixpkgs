bb6e47c9c253cb49d03a6c9bf5efab86c99f8f96
Vanilla <osu_Vanilla@126.com>
2021-10-29 19:18:03 +0800

    [+] local/nix/binaryCaches.nix: Add signature for nix-community.

--- local/nix/binaryCaches.nix
+++ local/nix/binaryCaches.nix
@@ -7,4 +7,9 @@
     "https://nix-community.cachix.org"
     "https://cache.nixos.org/"
   ];
+
+  # warning: substituter 'xxx' does not have a valid signature for path 'xxx'.
+  nix.binaryCachePublicKeys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
 }
