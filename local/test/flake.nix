738cadf81a060ccb12b31b98323f91e4347a9deb
Vanilla <osu_Vanilla@126.com>
2021-11-02 16:39:48 +0800

    [+] NixOS module dnscrypt-proxy2 is broken...?

--- local/test/flake.nix
+++ local/test/flake.nix
@@ -4,7 +4,8 @@
     # https://nixos.wiki/wiki/NixOS:nixos-rebuild_build-vm
     nixosConfigurations."NixOS-Laptop" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./xmonad.nix ./users-nixosvmtest.nix ./systemPackages.nix ];
+      # modules = [ ./xmonad.nix ./users-nixosvmtest.nix ./systemPackages.nix ];
+      modules = [{ services.dnscrypt-proxy2.enable = true; }];
     };
   };
 }
