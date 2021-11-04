c039f6c89b1979dc27a8c5efbc4f50df2ceeac97
Vanilla <osu_Vanilla@126.com>
2021-11-04 09:17:38 +0800

    [M] packages/resources/gitalias.nix: update sha256.

--- packages/resources/gitalias.nix
+++ packages/resources/gitalias.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation rec {
   # https://github.com/GitAlias/gitalias#install
   src = fetchurl {
     url = "https://raw.githubusercontent.com/GitAlias/${name}/master/${fileName}";
-    sha256 = "sha256-77GElkqy1EG57+xN8gidaaAtoRb+hPjJ2Usomwe4L3g=";
+    sha256 = "sha256-owo2Z24MraRisucY7PqOj4s619QTZMjSF+YQ5vU/Dhw=";
   };
 
   unpackPhase = "echo";
