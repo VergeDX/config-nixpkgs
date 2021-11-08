74dea7717964b3b4048b3e2d7b28c113d20bcf11
Vanilla <osu_Vanilla@126.com>
2021-11-08 22:20:01 +0800

    [-] local/boot.nix: Remove boot kernel params & extra modprobe config.

--- local/boot.nix
+++ local/boot.nix
@@ -10,21 +10,8 @@
 
   # https://gist.github.com/manuelmazzuola/4ffa90f5f5d0ddacda96#file-configuration-nix-L22
   boot.kernel.sysctl = { "kernel.sysrq" = 1; };
-  # https://github.com/NickCao/flakes/blob/baaa99e3b32ca01069443aa0466c6aeefe3620a4/nixos/local/configuration.nix#L79
-  boot.kernelParams = [
-    "pcie_aspm.policy=performance"
-    "nowatchdog"
-    "systemd.unified_cgroup_hierarchy=1"
-  ];
 
   boot.kernelPackages = pkgs.linuxPackages_xanmod;
-
-  # https://wiki.archlinux.org/title/intel_graphics
-  boot.extraModprobeConfig = ''
-    options i915 enable_guc=2
-    options i915 enable_fbc=1
-    options i915 fastboot=1
-  '';
 
   # https://github.com/slacka/WoeUSB/issues/299
   boot.supportedFilesystems = [ "ntfs" ];
