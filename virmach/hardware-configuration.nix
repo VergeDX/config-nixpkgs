57e4f6ba733085d8a21f6ce7f3e86786c6c77b0b
Vanilla <osu_Vanilla@126.com>
2021-11-09 18:44:53 +0800

    [M] virmach/hardware-configuration.nix: fileSystems by name.

--- virmach/hardware-configuration.nix
+++ virmach/hardware-configuration.nix
@@ -2,10 +2,6 @@
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/4fcdb3ce-fb5c-4616-89b6-4de59538a460";
-    fsType = "btrfs";
-  };
-
-  swapDevices = [{ device = "/dev/disk/by-uuid/5f467354-12d2-43c3-b0ad-95af6a734eb7"; }];
+  fileSystems."/" = { device = "/dev/vda1"; fsType = "btrfs"; };
+  swapDevices = [{ device = "/dev/vda2"; }];
 }
