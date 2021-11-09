a7dbd1290df88250436d15468a26d3bfe999b4da
Vanilla <osu_Vanilla@126.com>
2021-11-09 19:09:06 +0800

    [-] virmach/flake.nix: Remove useless comment.

--- virmach/flake.nix
+++ virmach/flake.nix
@@ -2,7 +2,6 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
   inputs.deploy-rs.url = "github:serokell/deploy-rs";
 
-  # https://nixos.wiki/wiki/Flakes#Using_nix_flakes_with_NixOS
   outputs = { self, nixpkgs, deploy-rs, ... }:
     let
       hostName = "VirMach-V2Ray";
