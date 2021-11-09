68c3a3be3de0cd48487cc9e763f8dddda81684d1
Vanilla <osu_Vanilla@126.com>
2021-11-09 22:11:16 +0800

    [+] virmach/boot.nix: Max SYN packages 128 -> 8192.

--- virmach/boot.nix
+++ virmach/boot.nix
@@ -7,5 +7,8 @@ in
   boot.kernel.sysctl = {
     # https://sysctl-explorer.net/net/ipv4/tcp_fastopen/
     "net.ipv4.tcp_fastopen" = 3;
+
+    # https://sysctl-explorer.net/net/ipv4/tcp_syncookies/
+    "net.ipv4.tcp_max_syn_backlog" = 128 * 8 * 8; # 8192
   };
 }
