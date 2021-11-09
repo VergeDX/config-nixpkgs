ed1d4294e536aa04a22b2cec01824649858ed40f
Vanilla <osu_Vanilla@126.com>
2021-11-09 20:40:31 +0800

    [+] virmach/boot.nix: Enable TFO level 3 - client & server.

--- virmach/boot.nix
+++ virmach/boot.nix
@@ -1,4 +1,8 @@
 { pkgs, ... }:
 {
   boot.kernelPackages = pkgs.linuxPackages_xanmod;
+  boot.kernel.sysctl = {
+    # https://sysctl-explorer.net/net/ipv4/tcp_fastopen/
+    "net.ipv4.tcp_fastopen" = 3;
+  };
 }
