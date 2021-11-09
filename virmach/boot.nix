322aab837d5fe91ea3988e4f859ce0dc64c323ac
Vanilla <osu_Vanilla@126.com>
2021-11-09 21:35:53 +0800

    [+] virmach/boot.nix: Use xanmod kernel from unstable.

--- virmach/boot.nix
+++ virmach/boot.nix
@@ -1,6 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, inputs, system, ... }:
+let pkgs-unstable = import inputs.nixos-unstable { inherit system; };
+in
 {
-  boot.kernelPackages = pkgs.linuxPackages_xanmod;
+  boot.kernelPackages = pkgs-unstable.linuxPackages_xanmod;
+
   boot.kernel.sysctl = {
     # https://sysctl-explorer.net/net/ipv4/tcp_fastopen/
     "net.ipv4.tcp_fastopen" = 3;
--- virmach/flake.nix
+++ virmach/flake.nix
@@ -1,15 +1,16 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
   inputs.deploy-rs.url = "github:serokell/deploy-rs";
+  inputs.nixos-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, deploy-rs, ... }:
+  outputs = { self, nixpkgs, deploy-rs, ... }@inputs:
     let
       hostName = "VirMach-V2Ray";
       system = "x86_64-linux";
     in
     rec {
       nixosConfigurations."${hostName}" = nixpkgs.lib.nixosSystem {
-        inherit system; specialArgs = { inherit hostName; };
+        inherit system; specialArgs = { inherit hostName inputs system; };
         modules = [ ./configuration.nix ];
       };
 
