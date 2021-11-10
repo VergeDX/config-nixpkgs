73712a40e815f022db9fd034dc1a2a059819226a
Vanilla <osu_Vanilla@126.com>
2021-11-10 19:07:18 +0800

    [+] virmach/services/v2ray.nix: MTProto on 443.

--- virmach/services/v2ray.nix
+++ virmach/services/v2ray.nix
@@ -18,14 +18,33 @@
       # https://github.com/v2ray/v2ray-core/issues/826
       # https://toutyrater.github.io/advanced/httpfake.html
       "streamSettings"."tcpSettings"."header"."type" = "http";
-    }];
+    }
+
+      {
+        "tag" = "tg-in";
+        "port" = 443;
+        "protocol" = "mtproto";
+        "settings" = { "users" = [{ "secret" = "b0cbcef5a486d9636472ac27f8e11a9d"; }]; };
+      }];
 
     "outbounds" = [{
       "protocol" = "freedom";
       "settings" = { };
+    }
+      {
+        "tag" = "tg-out";
+        "protocol" = "mtproto";
+        "settings" = { };
+      }];
+
+    # https://www.v2ray.com/chapter_02/protocols/mtproto.html#sample
+    "routing"."rules" = [{
+      "type" = "field";
+      "inboundTag" = [ "tg-in" ];
+      "outboundTag" = "tg-out";
     }];
   };
 
-  networking.firewall.allowedTCPPorts = [ 10086 ];
-  networking.firewall.allowedUDPPorts = [ 10086 ];
+  networking.firewall.allowedTCPPorts = [ 10086 443 ];
+  networking.firewall.allowedUDPPorts = [ 10086 443 ];
 }
