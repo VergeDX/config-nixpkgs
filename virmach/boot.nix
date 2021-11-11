4b5864f667418652b29460755a5beddb1c42a539
Vanilla <osu_Vanilla@126.com>
2021-11-11 08:47:50 +0800

    [+] virmach/boot.nix: somaxconn = 16k as CF.

--- virmach/boot.nix
+++ virmach/boot.nix
@@ -8,5 +8,8 @@
 
     # https://sysctl-explorer.net/net/ipv4/tcp_syncookies/
     "net.ipv4.tcp_max_syn_backlog" = 128 * 8 * 8; # 8192
+
+    # https://blog.cloudflare.com/syn-packet-handling-in-the-wild/
+    "net.core.somaxconn" = 16 * 1024;
   };
 }
