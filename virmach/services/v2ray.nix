fdc2ecb6791110d79eed9b8adf8647a428b5ba17
Vanilla <osu_Vanilla@126.com>
2021-11-11 19:42:35 +0800

    [+] virmach/services/v2ray.nix: Hide id & secret.

--- virmach/services/v2ray.nix
+++ virmach/services/v2ray.nix
@@ -1,45 +1,55 @@
-{ ... }:
+{ lib, pkgs, ... }:
+# https://www.v2ray.com/chapter_02/01_overview.html
+let config_JSON = lib.strings.toJSON {
+  # https://www.v2ray.com/chapter_02/01_overview.html#logobject
+  # "log"."loglevel" = "info";
+
+  # https://developers.google.com/speed/public-dns/docs/doh
+  "dns" = { "servers" = [ "https://dns.google/dns-query" ]; };
+
+  "inbounds" = [
+    {
+      "port" = 10086;
+      "protocol" = "vmess";
+      "settings" = { "clients" = [{ "id" = "@id@"; }]; };
+
+      # https://github.com/v2ray/v2ray-core/issues/826
+      # https://toutyrater.github.io/advanced/httpfake.html
+      "streamSettings"."tcpSettings"."header"."type" = "http";
+    }
+    {
+      "tag" = "tg-in";
+      "port" = 443;
+      "protocol" = "mtproto";
+      "settings" = { "users" = [{ "secret" = "@secret@"; }]; };
+    }
+  ];
+
+  "outbounds" = [
+    { "protocol" = "freedom"; "settings" = { }; }
+    { "tag" = "tg-out"; "protocol" = "mtproto"; "settings" = { }; }
+  ];
+
+  # https://www.v2ray.com/chapter_02/protocols/mtproto.html#sample
+  "routing"."rules" = [{
+    "type" = "field";
+    "inboundTag" = [ "tg-in" ];
+    "outboundTag" = "tg-out";
+  }];
+};
+in
 {
   services.v2ray.enable = true;
+  services.v2ray.configFile = "/etc/v2ray/Vanilla-V2Ray.json";
 
-  # https://www.v2ray.com/chapter_02/01_overview.html
-  services.v2ray.config = {
-    # https://www.v2ray.com/chapter_02/01_overview.html#logobject
-    # "log"."loglevel" = "info";
-
-    # https://developers.google.com/speed/public-dns/docs/doh
-    "dns" = { "servers" = [ "https://dns.google/dns-query" ]; };
-
-    "inbounds" = [
-      {
-        "port" = 10086;
-        "protocol" = "vmess";
-        "settings" = { "clients" = [{ "id" = "b831381d-6324-4d53-ad4f-8cda48b30811"; }]; };
-
-        # https://github.com/v2ray/v2ray-core/issues/826
-        # https://toutyrater.github.io/advanced/httpfake.html
-        "streamSettings"."tcpSettings"."header"."type" = "http";
-      }
-      {
-        "tag" = "tg-in";
-        "port" = 443;
-        "protocol" = "mtproto";
-        "settings" = { "users" = [{ "secret" = "b0cbcef5a486d9636472ac27f8e11a9d"; }]; };
-      }
-    ];
-
-    "outbounds" = [
-      { "protocol" = "freedom"; "settings" = { }; }
-      { "tag" = "tg-out"; "protocol" = "mtproto"; "settings" = { }; }
-    ];
-
-    # https://www.v2ray.com/chapter_02/protocols/mtproto.html#sample
-    "routing"."rules" = [{
-      "type" = "field";
-      "inboundTag" = [ "tg-in" ];
-      "outboundTag" = "tg-out";
-    }];
-  };
+  system.activationScripts."Vanilla-V2Ray.json".deps = [ "agenixRoot" ];
+  system.activationScripts."Vanilla-V2Ray.json".text = ''
+    rm -r /etc/v2ray && mkdir /etc/v2ray || true
+    echo '${config_JSON}' > /etc/v2ray/Vanilla-V2Ray.json
+    id="$(cat /run/secrets/v2ray/id)" && secret="$(cat /run/secrets/v2ray/secret)"
+    ${pkgs.gnused}/bin/sed -i "s/@id@/$id/g" /etc/v2ray/Vanilla-V2Ray.json
+    ${pkgs.gnused}/bin/sed -i "s/@secret@/$secret/g" /etc/v2ray/Vanilla-V2Ray.json
+  '';
 
   networking.firewall.allowedTCPPorts = [ 10086 443 ];
   networking.firewall.allowedUDPPorts = [ 10086 443 ];
