fd5f29500d48c1b6b4756283e87fd59cd1ce2a45
Vanilla <osu_Vanilla@126.com>
2021-11-11 09:17:10 +0800

    [M] virmach/flake.nix: Reformat modules.

--- virmach/flake.nix
+++ virmach/flake.nix
@@ -17,17 +17,12 @@
         inherit system;
         specialArgs = { inherit hostName pkgs-unstable; };
 
-        modules = [
-          ./configuration.nix
-          { nixpkgs.overlays = [ (self: super: { v2ray = pkgs-unstable.v2ray; }) ]; }
-
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users."root" = import ./home.nix;
-          }
-        ];
+        modules = [ ./configuration.nix ]
+          ++ [{ nixpkgs.overlays = [ (self: super: { v2ray = pkgs-unstable.v2ray; }) ]; }]
+          ++ [ home-manager.nixosModules.home-manager ]
+          ++ [{ home-manager.useGlobalPkgs = true; }]
+          ++ [{ home-manager.useUserPackages = true; }]
+          ++ [{ home-manager.users."root" = import ./home.nix; }];
       };
 
       deploy.nodes."${hostName}" = rec {
