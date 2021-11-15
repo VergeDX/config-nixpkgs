2757e0fe9dd2072bd04135ac1c24bd2c89299fd6
Vanilla <osu_Vanilla@126.com>
2021-11-15 21:19:56 +0800

    [+] virmach/services/telegraf.nix: Add Fail2ban config.

--- virmach/services/telegraf.nix
+++ virmach/services/telegraf.nix
@@ -1,15 +1,29 @@
 { config, wget, lib, pkgs, ... }:
 {
   services.telegraf.enable = true;
-  services.telegraf.environmentFiles =
-    [ "/run/secrets/telegraf/INFLUX_TOKEN.env" ];
+
+  services.telegraf.environmentFiles = [ "/run/secrets/telegraf/INFLUX_TOKEN.env" ];
+  systemd.services."telegraf".serviceConfig."Environment" =
+    [ "INFLUX_HOST=https://vanilla.cyunrei.moe" "INFLUX_ORG=NixOS" "INFLUX_BUCKET=default" ];
+  systemd.services."telegraf".path = [ pkgs.fail2ban ];
 
   # NixOS - nixpkgs: nixos/modules/services/monitoring/telegraf.nix
   systemd.services."telegraf".serviceConfig."ExecStart" = lib.mkForce
-    # https://unix.stackexchange.com/questions/216780
-    ("${pkgs.bash}/bin/bash" + " " + "-c" + " " + "\"" +
-      "${config.services.telegraf.package}/bin/telegraf" + " " +
-      "-config" + " " + "$$(cat /run/secrets/telegraf/config_url/system)" + "\"");
+    "${pkgs.telegraf}/bin/telegraf --config-directory /etc/telegraf/telegraf.d/";
+
+  # https://github.com/influxdata/telegraf/issues/491
+  system.activationScripts."Build-Telegraf-config".deps = [ "agenixRoot" ];
+  system.activationScripts."Build-Telegraf-config".text = ''
+    rm -rf /etc/telegraf/ && true
+    mkdir -p /etc/telegraf/telegraf.d && cd "$_"
+
+    # https://docs.influxdata.com/influxdb/cloud/security/tokens/create-token/
+    export $(cat /run/secrets/telegraf/INFLUX_TOKEN.env)
+    ${pkgs.curl}/bin/curl  $(cat /run/secrets/telegraf/config_url/system) \
+      --header "Authorization: Token $INFLUX_TOKEN" > ../telegraf.conf
+    ${pkgs.curl}/bin/curl  $(cat /run/secrets/telegraf/config_url/fail2ban) \
+      --header "Authorization: Token $INFLUX_TOKEN" > fail2ban.conf
+  '';
 
   systemd.services."telegraf".serviceConfig."AmbientCapabilities" = [ "CAP_DAC_OVERRIDE" ];
   systemd.services."telegraf".serviceConfig."CapabilityBoundingSet" = [ "CAP_DAC_OVERRIDE" ];
