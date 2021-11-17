9b9860acd73fd50704dc079d472982ee9af23fda
Vanilla <osu_Vanilla@126.com>
2021-11-17 10:40:29 +0800

    [+] local/services/hbase.nix: Install HBase too.

--- local/services/hbase.nix
+++ local/services/hbase.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
 {
   services.hbase.enable = true;
   services.hbase.package = pkgs.hbase;
@@ -7,4 +7,7 @@
     # NixOS - nixpkgs: nixos/modules/services/databases/hbase.nix
     "JAVA_HOME" = lib.mkForce "${pkgs.jre8}";
   };
+
+  environment.systemPackages =
+    [ config.services.hbase.package ];
 }
