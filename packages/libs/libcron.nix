df2eb59bc3189c7f4bcc93dede1e8bfa01f98c62
Vanilla <osu_Vanilla@126.com>
2021-11-22 17:47:36 +0800

    [+] packages/libs/libcron.nix: Use CMakeLists.txt.

--- packages/libs/libcron.nix
+++ packages/libs/libcron.nix
@@ -9,13 +9,12 @@ stdenv.mkDerivation rec {
     hash = "sha256-MXmEHUJOZNnbJNMfhDKvl6fwVvPG3MjCaER0xOwRX2I=";
   };
 
-  nativeBuildInputs = [ cmake ];
-
-  # https://github.com/tindy2013/subconverter/blob/v0.7.1/scripts/build.alpine.release.sh#L34
-  installPhase = ''
-    mkdir -p $out/lib/ $out/include/libcron/ $out/include/date/
-    install -m644 /build/libcron/libcron/out/Release/liblibcron.a $out/lib/
-    install -m644 /build/libcron/libcron/include/libcron/* $out/include/libcron/
-    install -m644 /build/libcron/libcron/externals/date/include/date/* $out/include/date/
+  # https://github.com/PerMalmberg/libcron/pull/23/files
+  patchPhase = ''
+    echo 'install(TARGETS libcron DESTINATION lib)' >> CMakeLists.txt
+    echo 'install(DIRECTORY libcron/include/libcron DESTINATION include)' >> CMakeLists.txt
+    echo 'install(DIRECTORY libcron/externals/date/include/date DESTINATION include)' >> CMakeLists.txt
   '';
+
+  nativeBuildInputs = [ cmake ];
 }
