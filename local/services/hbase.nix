6c2a9100f913c6f09a81f9021e13431b6d8630ed
Vanilla <osu_Vanilla@126.com>
2021-11-23 11:28:17 +0800

    [-] local/services/hbase.nix: Remove module replacement.

--- local/services/hbase.nix
+++ local/services/hbase.nix
@@ -1,8 +1,5 @@
-{ pkgs, lib, config, inputs, ... }:
+{ pkgs, lib, config, ... }:
 {
-  disabledModules = [ "services/databases/hbase.nix" ];
-  imports = [ "${inputs.nixpkgs-master}/nixos/modules/services/databases/hbase.nix" ];
-
   services.hbase.enable = true;
   services.hbase.package = pkgs.hbase;
   services.hbase.settings."hbase.master.info.port" = 60010;
