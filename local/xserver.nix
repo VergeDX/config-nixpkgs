501e5bc2668b54492458a28765f1fc69ca99b767
Vanilla <osu_Vanilla@126.com>
2021-11-24 09:50:21 +0800

    [+] local/xserver.nix: overlays for gnome new screenshot ui.

--- local/xserver.nix
+++ local/xserver.nix
@@ -1,4 +1,29 @@
-{ pkgs, config, ... }:
+{ pkgs, config, lib, ... }:
+let gnome-shell-new = pkgs: pkgs.gnome.gnome-shell.overrideAttrs (old: {
+  patches = old.patches ++ lib.singleton (pkgs.fetchurl {
+    url = "https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1954/diffs.patch";
+    hash = "sha256-qnvKaNDIzElLfFCuZLldSJloSWd/InZ9xVk8DqpPX1Y=";
+  });
+}); in
+let gnome-settings-daemon-new = pkgs: pkgs.gnome.gnome-settings-daemon.overrideAttrs (old: {
+  patches = old.patches ++ lib.singleton (pkgs.fetchurl {
+    url = "https://gitlab.gnome.org/GNOME/gnome-settings-daemon/-/merge_requests/267/diffs.patch";
+    hash = "sha256-/YZ1ARoK8ZHY/vuS7ZKf6desCr4ny09e/83f2YRbITg=";
+  });
+}); in
+let gnome-control-center-new = pkgs: pkgs.gnome.gnome-control-center.overrideAttrs (old: {
+  patches = old.patches ++ lib.singleton (pkgs.fetchurl {
+    url = "https://gitlab.gnome.org/GNOME/gnome-control-center/-/merge_requests/1075/diffs.patch";
+    hash = "sha256-nmjRJMWg8+huwLuiwgOq6kCNvDKCPnM/+Q8braZcYg4=";
+  });
+}); in
+let mutter-new = pkgs: pkgs.gnome.mutter.overrideAttrs (old: {
+  src = pkgs.fetchgit {
+    url = "https://gitlab.gnome.org/YaLTeR/mutter";
+    branchName = "screenshot-ui-temp";
+    hash = "sha256-GaKrXObJ8T/R0NaEKbGZL8JTY0eCCNp+27vdmH7WxP8=";
+  };
+}); in
 {
   # Enable the X11 windowing system.
   services.xserver.enable = true;
@@ -35,4 +60,17 @@
   # https://github.com/NixOS/nixpkgs/pull/107850
   systemd.services."display-manager".preStart =
     "cp /home/vanilla/.config/monitors.xml /run/gdm/.config/monitors.xml";
+
+  # https://github.com/OttoAllmendinger/gnome-shell-screenshot/issues/158
+  # https://copr.fedorainfracloud.org/coprs/yalter/gnome-shell-screenshot-ui/
+  nixpkgs.overlays = [
+    (self: super: {
+      gnome = super.gnome // {
+        gnome-shell = (gnome-shell-new super);
+        gnome-settings-daemon = (gnome-settings-daemon-new super);
+        gnome-control-center = (gnome-control-center-new super);
+        mutter = (mutter-new super);
+      };
+    })
+  ];
 }
