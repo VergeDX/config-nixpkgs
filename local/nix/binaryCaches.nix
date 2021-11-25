b3a6a2a3e34098fd2cb947c0e534b9c304c4a6f4
Vanilla <osu_Vanilla@126.com>
2021-11-25 16:46:32 +0800

    [+] local/nix/binaryCaches.nix: Enable all mirror site of Nix.

--- local/nix/binaryCaches.nix
+++ local/nix/binaryCaches.nix
@@ -2,8 +2,8 @@
 {
   # sudo nixos-rebuild switch --option substituters https://cache.nixos.org/
   nix.binaryCaches = [
-    # "https://mirrors.bfsu.edu.cn/nix-channels/store"
-    # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+    "https://mirrors.bfsu.edu.cn/nix-channels/store"
+    "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
     "https://mirror.sjtu.edu.cn/nix-channels/store"
     "https://nix-community.cachix.org"
     "https://cache.nixos.org/"
