6c8a8f60d73db39707f58e061fa95eb31397a9c6
Vanilla <osu_Vanilla@126.com>
2021-11-25 17:15:28 +0800

    [+] packages/cutefish/cutefishos-dock.nix.

---
+++ packages/cutefish/cutefishos-dock.nix
@@ -0,0 +1,22 @@
+{ stdenv, fetchgit, cmake, libsForQt5, callPackage, ... }:
+let cutefishos-fishui = callPackage ./cutefishos-fishui.nix { }; in
+stdenv.mkDerivation rec {
+  pname = "cutefishos-dock";
+  version = "0.5";
+
+  src = fetchgit {
+    url = "https://github.com/cutefishos/dock";
+    rev = "${version}";
+    hash = "sha256-SFvxDoaZNtlvHFjDbEdLXQkz2vzd7vSKqI7QrwjxIZ8=";
+  };
+
+  nativeBuildInputs = [ cmake libsForQt5.wrapQtAppsHook ];
+  buildInputs = with libsForQt5; [ qtx11extras qttools qtquickcontrols2 kwindowsystem ]
+    ++ [ cutefishos-fishui libsForQt5.qtgraphicaleffects ];
+
+  # https://github.com/cutefishos/dock/blob/0.5/CMakeLists.txt#L62
+  patchPhase = ''
+    sed -i 's@/usr@usr@g' CMakeLists.txt
+    sed -i 's@/etc@etc@g' CMakeLists.txt
+  '';
+}
