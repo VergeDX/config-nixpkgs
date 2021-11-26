28fde8db3c9283a4a5676f555ba0e5411ba591e2
Vanilla <osu_Vanilla@126.com>
2021-11-26 18:11:44 +0800

    [+] packages/cutefish/cutefishos-libcutefish.nix.

---
+++ packages/cutefish/cutefishos-libcutefish.nix
@@ -0,0 +1,23 @@
+{ stdenv, fetchgit, cmake, libsForQt5, callPackage, ... }:
+let cutefishos-fishui = callPackage ./cutefishos-fishui.nix { }; in
+stdenv.mkDerivation rec {
+  pname = "cutefishos-libcutefish";
+  version = "0.5";
+
+  src = fetchgit {
+    url = "https://github.com/cutefishos/libcutefish";
+    rev = "${version}";
+    hash = "sha256-os47UB/qyhiNu6MIihWI7bBWYLM+n0IrSdZM85Fys1o=";
+  };
+
+  nativeBuildInputs = [ cmake ];
+  buildInputs = (with libsForQt5; [ qtbase qtquickcontrols qtquickcontrols2 ])
+    ++ (with libsForQt5; [ bluez-qt networkmanager-qt modemmanager-qt ])
+    ++ (with libsForQt5; [ libkscreen kio qtsensors ]);
+
+  dontWrapQtApps = true;
+
+  # https://github.com/cutefishos/libcutefish/blob/0.5/CMakeLists.txt#L25
+  patchPhase = "sed -i '25,28d' CMakeLists.txt";
+  cmakeFlags = [ "-DINSTALL_QMLDIR=lib/qt-${libsForQt5.qtbase.version}/qml" ];
+}
