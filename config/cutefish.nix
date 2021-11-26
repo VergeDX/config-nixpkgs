e23161567444c771c6130d88356ad1c35ff1bd85
Vanilla <osu_Vanilla@126.com>
2021-11-26 20:11:33 +0800

    [+] config/cutefish.nix: Add cutefish packages.

--- config/cutefish.nix
+++ config/cutefish.nix
@@ -3,5 +3,8 @@
   home.packages = [
     (pkgs.callPackage ../packages/cutefish/cutefishos-dock.nix { })
     (pkgs.callPackage ../packages/cutefish/cutefishos-filemanager.nix { })
+    (pkgs.callPackage ../packages/cutefish/cutefishos-calculator.nix { })
+    (pkgs.callPackage ../packages/cutefish/cutefishos-launcher.nix { })
+    (pkgs.callPackage ../packages/cutefish/cutefishos-terminal.nix { })
   ];
 }
