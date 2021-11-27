13576260ae9c9e63032f04f89f30a6c5d02c47e4
Vanilla <osu_Vanilla@126.com>
2021-11-27 09:48:35 +0800

    [M] config/cutefish.nix: Call cutefishos/default.nix instead.

--- config/cutefish.nix
+++ config/cutefish.nix
@@ -1,10 +1,11 @@
 { pkgs, ... }:
+let cutefishos = pkgs.callPackage ../packages/cutefishos { }; in
 {
-  home.packages = [
-    (pkgs.callPackage ../packages/cutefish/cutefishos-dock.nix { })
-    (pkgs.callPackage ../packages/cutefish/cutefishos-filemanager.nix { })
-    (pkgs.callPackage ../packages/cutefish/cutefishos-calculator.nix { })
-    (pkgs.callPackage ../packages/cutefish/cutefishos-launcher.nix { })
-    (pkgs.callPackage ../packages/cutefish/cutefishos-terminal.nix { })
+  home.packages = with cutefishos; [
+    dock
+    filemanager
+    calculator
+    launcher
+    terminal
   ];
 }
