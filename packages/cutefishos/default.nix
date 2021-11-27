dc7f68a9ff2e60114afbbff28ad7480464bbdaea
Vanilla <osu_Vanilla@126.com>
2021-11-27 11:07:39 +0800

    [+] packages/cutefishos/statusbar.nix.

--- packages/cutefishos/default.nix
+++ packages/cutefishos/default.nix
@@ -8,6 +8,7 @@
   launcher = callPackage ./launcher.nix { };
   libcutefish = callPackage ./libcutefish.nix { };
   screenshot = callPackage ./screenshot.nix { };
+  statusbar = callPackage ./statusbar.nix { };
   terminal = callPackage ./terminal.nix { };
   wallpapers = callPackage ./wallpapers.nix { };
 }
