3d16155cb6dc7ba158ec24c46078b6357fef13e6
Vanilla <osu_Vanilla@126.com>
2021-11-29 16:25:22 +0800

    [O] config/shells.nix: Use nur.repos.linyinfeng.fishPlugins instead.

--- config/shells.nix
+++ config/shells.nix
@@ -1,15 +1,4 @@
 { programs, home, pkgs, ... }:
-let plugin-git = pkgs.fishPlugins.buildFishPlugin rec {
-  pname = "plugin-git";
-  version = "bc6caff";
-
-  src = pkgs.fetchgit {
-    url = "https://github.com/jhillyerd/${pname}";
-    rev = "${version}38fdcac309fddac6651c13cd217d5a382";
-    sha256 = "sha256-jodMn2KfSnO6xUgOvMHYXU4PUAEUS5skVuNcBJQYzyU=";
-  };
-};
-in
 {
   programs.fish.enable = true;
   # https://starship.rs/zh-CN/config/#%E6%8F%90%E7%A4%BA%E7%AC%A6
@@ -30,11 +19,9 @@ in
   programs.fish.shellAliases = { top = "bpytop"; };
 
   # https://github.com/franciscolourenco/done#for-linux-set-the-urgency-level-for-notifications-sent-via-notify-send-low-normal-critical-the-default-is-normal-for-regular-commands-and-critical-for-failed-commands
-  # https://github.com/jhillyerd/plugin-git/blob/master/hooks/install.fish
   programs.fish.interactiveShellInit = ''
     set -U __done_notification_urgency_level low
     set -U __done_notification_urgency_level_failure normal
-    source ${plugin-git}/share/fish/vendor_functions.d/__git.init.fish && __git.init
   '';
 
   home.packages = [
@@ -50,25 +37,14 @@ in
     pkgs.meslo-lgs-nf
 
     pkgs.bubblewrap
-  ] ++ (with pkgs.fishPlugins; [ done pisces bfish ]);
+  ] ++ (with pkgs.fishPlugins; [ done pisces bfish ])
+  ++ [
+    pkgs.nur.repos.linyinfeng.fishPlugins.plugin-git
+    pkgs.nur.repos.linyinfeng.fishPlugins.plugin-bang-bang
+    pkgs.nur.repos.linyinfeng.fishPlugins.replay-fish
+  ];
 
   programs.fish.plugins = [
-    {
-      name = "plugin-bang-bang";
-      src = pkgs.fetchgit {
-        url = "https://github.com/oh-my-fish/plugin-bang-bang";
-        rev = "f969c618301163273d0a03d002614d9a81952c1e";
-        sha256 = "sha256-A8ydBX4LORk+nutjHurqNNWFmW6LIiBPQcxS3x4nbeQ=";
-      };
-    }
-    {
-      name = "replay.fish";
-      src = pkgs.fetchgit {
-        url = "https://github.com/jorgebucaran/replay.fish";
-        rev = "1.2.0";
-        sha256 = "sha256-Q/9YVdiRSJw1SdcfQv2h7Lj6EyFustRk+kmh1eRRQ6k=";
-      };
-    }
     {
       name = "nix-completions.fish";
       src = pkgs.fetchgit {
