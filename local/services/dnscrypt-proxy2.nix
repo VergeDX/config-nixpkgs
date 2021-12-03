98dd3c490a779070b08b8e792b4598524f1db731
Vanilla <osu_Vanilla@126.com>
2021-12-03 09:05:01 +0800

    [+] local/services/dnscrypt-proxy2.nix: += dnspod-doh.

--- local/services/dnscrypt-proxy2.nix
+++ local/services/dnscrypt-proxy2.nix
@@ -8,6 +8,6 @@
   services.dnscrypt-proxy2.enable = true;
   services.dnscrypt-proxy2.settings = {
     # https://download.dnscrypt.info/resolvers-list/v2/public-resolvers.md
-    server_names = [ "alidns-doh" ];
+    server_names = [ "alidns-doh" "dnspod-doh" ];
   };
 }
