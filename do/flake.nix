750d0531ac5dcd0be7abb8bbda7eb14f6b1c5260
Vanilla <osu_Vanilla@126.com>
2021-12-06 13:02:57 +0800

    [M] do/flake.nix: Use flake-utils instead.

--- do/flake.nix
+++ do/flake.nix
@@ -9,30 +9,36 @@
     };
 
     deploy-rs.url = "github:serokell/deploy-rs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, nixos-generators, deploy-rs, ... }: {
-    packages.x86_64-linux = {
-      do = nixos-generators.nixosGenerate {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        # https://docs.digitalocean.com/products/images/custom-images/#image-requirements
-        modules = [ ./services/cloud-init.nix ./services/openssh.nix ] ++ [ ./users.nix ];
-        format = "do";
-      };
-    };
+  outputs = { self, nixpkgs, nixos-generators, deploy-rs, flake-utils, ... }:
+    # https://github.com/numtide/flake-utils#example
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      rec {
+        packages = flake-utils.lib.flattenTree {
+          do = nixos-generators.nixosGenerate {
+            pkgs = nixpkgs.legacyPackages.x86_64-linux;
+            # https://docs.digitalocean.com/products/images/custom-images/#image-requirements
+            modules = [ ./services/cloud-init.nix ./services/openssh.nix ] ++ [ ./users.nix ];
+            format = "do";
+          };
+        };
 
-    deploy.nodes."nixos" = rec {
-      hostname = "134.209.111.162";
-      sshUser = "root";
+        deploy.nodes."nixos" = rec {
+          hostname = "134.209.111.162";
+          sshUser = "root";
 
-      profiles.system = {
-        user = "${sshUser}";
-        path = deploy-rs.lib."x86_64-linux".activate.nixos (nixpkgs.lib.nixosSystem
-          { system = "x86_64-linux"; modules = [ ./configuration.nix ]; });
+          profiles.system = {
+            user = "${sshUser}";
+            path = deploy-rs.lib."x86_64-linux".activate.nixos (nixpkgs.lib.nixosSystem
+              { system = "x86_64-linux"; modules = [ ./configuration.nix ]; });
 
-        fastConnection = true;
-        autoRollback = false;
-      };
-    };
-  };
+            fastConnection = true;
+            autoRollback = false;
+          };
+        };
+      }
+    );
 }
