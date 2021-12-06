6d52c350a6aae2052e6f7af629dd9747f40ce711
Vanilla <osu_Vanilla@126.com>
2021-12-06 16:21:33 +0800

    [+] do/services/fail2ban.nix.

--- do/configuration.nix
+++ do/configuration.nix
@@ -1,6 +1,7 @@
 { inputs, pkgs, ... }:
 {
   imports = [ ./boot.nix ] ++ [{ users.users."root".shell = pkgs.fish; }] ++
-    [ "${inputs.nixpkgs}/nixos/modules/virtualisation/digital-ocean-config.nix" ];
+    [ "${inputs.nixpkgs}/nixos/modules/virtualisation/digital-ocean-config.nix" ]
+    ++ [ ../virmach/services/fail2ban.nix ];
   networking.hostName = "nixos";
 }
