71ba6fb95b74de69ce99a659daa4462b938cfe12
Vanilla <osu_Vanilla@126.com>
2021-12-06 11:55:01 +0800

    [+] do/flake.nix: Add support for deploy-rs.

--- do/configuration.nix
+++ do/configuration.nix
@@ -2,4 +2,7 @@
 {
   imports = [ ./services/cloud-init.nix ./services/openssh.nix ]
     ++ [ ./users.nix ];
+
+  fileSystems = { "/" = { device = "/dev/vda1"; fsType = "ext4"; }; };
+  boot.loader.grub.device = "/dev/vda";
 }
--- do/flake.nix
+++ do/flake.nix
@@ -7,15 +7,29 @@
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    deploy-rs.url = "github:serokell/deploy-rs";
   };
 
-  outputs = { self, nixpkgs, nixos-generators, ... }: {
+  outputs = { self, nixpkgs, nixos-generators, deploy-rs, ... }: {
     packages.x86_64-linux = {
       do = nixos-generators.nixosGenerate {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         # https://docs.digitalocean.com/products/images/custom-images/#image-requirements
         modules = [ ./services/cloud-init.nix ./services/openssh.nix ] ++ [ ./users.nix ];
         format = "do";
+      };
+    };
+
+    deploy.nodes."nixos" = rec {
+      hostname = "134.209.111.162";
+      sshUser = "root";
+
+      profiles.system = {
+        user = "${sshUser}";
+        path = deploy-rs.lib."x86_64-linux".activate.nixos (nixpkgs.lib.nixosSystem
+          { system = "x86_64-linux"; modules = [ ./configuration.nix ]; });
+        fastConnection = true;
       };
     };
   };
