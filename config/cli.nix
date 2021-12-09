9a929f7cb25bc689a4bcc397bc1bf7738aabc7c2
Vanilla <osu_Vanilla@126.com>
2021-12-09 17:39:54 +0800

    [+] config/cli.nix: Two iconv patches from Fedora, for pkgs.unzip.

--- config/cli.nix
+++ config/cli.nix
@@ -1,7 +1,23 @@
 { home, pkgs, ... }:
 {
   home.packages = [
-    pkgs.unzip
+    # https://gitlab.gnome.org/GNOME/file-roller/-/issues/137
+    (pkgs.unzip.overrideAttrs (old: rec {
+      # https://src.fedoraproject.org/rpms/unzip/tree/rawhide
+      baseUrl = "https://src.fedoraproject.org/rpms/unzip/raw/rawhide/f";
+      # https://github.com/archlinux/svntogit-packages/blob/packages/unzip/trunk/PKGBUILD
+      patches = old.patches ++ [
+        (pkgs.fetchurl {
+          url = "${baseUrl}/unzip-6.0-alt-iconv-utf8.patch";
+          hash = "sha256-5kyd2zjC59CL24DFl/Mu6WDhj76MuYLkRLHs4DrJXOw=";
+        })
+        (pkgs.fetchurl {
+          url = "${baseUrl}/unzip-6.0-alt-iconv-utf8-print.patch";
+          hash = "sha256-uZDY2NjgJ3eZlIShMhcGZupzaoZfmtgdplHcY3JUdf8=";
+        })
+      ];
+    }))
+
     pkgs.scrcpy
     pkgs.glxinfo
     pkgs.mr
