5d9878a5b0f84a5e2257f8f21f761afc25fa6582
Vanilla <osu_Vanilla@126.com>
2021-12-22 18:33:11 +0800

    [+] config/zsh.nix: programs.zsh.prezto.enable = true;.

--- config/zsh.nix
+++ config/zsh.nix
@@ -15,6 +15,8 @@
 
   # https://nix-community.github.io/home-manager/options.html#opt-programs.zsh.oh-my-zsh
   programs.zsh.oh-my-zsh.enable = true;
+  # https://nix-community.github.io/home-manager/options.html#opt-programs.zsh.prezto
+  programs.zsh.prezto.enable = true;
 
   # https://nix-community.github.io/home-manager/options.html#opt-programs.starship.enable
   programs.starship.enableZshIntegration = false;
