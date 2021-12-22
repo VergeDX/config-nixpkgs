c0ebdba08f0e332ae61237fa0ee7489ad12785c4
Vanilla <osu_Vanilla@126.com>
2021-12-22 21:57:55 +0800

    [+] local/nix/binaryCaches.nix: Add linyinfeng cachix.

--- local/nix/binaryCaches.nix
+++ local/nix/binaryCaches.nix
@@ -5,12 +5,16 @@
     "https://mirrors.bfsu.edu.cn/nix-channels/store"
     "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
     "https://mirror.sjtu.edu.cn/nix-channels/store"
+
     "https://nix-community.cachix.org"
+    "https://linyinfeng.cachix.org"
+
     "https://cache.nixos.org/"
   ];
 
   # warning: substituter 'xxx' does not have a valid signature for path 'xxx'.
   nix.binaryCachePublicKeys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "linyinfeng.cachix.org-1:sPYQXcNrnCf7Vr7T0YmjXz5dMZ7aOKG3EqLja0xr9MM="
   ];
 }
