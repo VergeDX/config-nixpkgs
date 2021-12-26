c8a884e48fac9a57398284a613ca10cbf0cd58aa
Vanilla <osu_Vanilla@126.com>
2021-12-26 15:10:38 +0800

    [+] config/zsh.nix: Config WakaTime plugin for ZSH.

--- config/zsh.nix
+++ config/zsh.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.zsh.enable = true;
 
@@ -20,4 +20,22 @@
 
   # https://nix-community.github.io/home-manager/options.html#opt-programs.starship.enable
   programs.starship.enableZshIntegration = false;
+
+  # https://wakatime.com/terminal#install-zsh
+  programs.zsh.plugins = [
+    rec {
+      name = "wakatime-zsh-plugin";
+      # https://nix-community.github.io/home-manager/options.html#opt-programs.zsh.plugins
+      file = "wakatime.plugin.zsh";
+      src = pkgs.fetchgit {
+        url = "https://github.com/sobolevn/${name}";
+        hash = "sha256-YDfbbugRXmfC4NVXSvSP4Fhm92t0KJKcoFnfWj4CWN0=";
+      };
+    }
+  ];
+
+  # https://nix-community.github.io/home-manager/options.html#opt-programs.zsh.sessionVariables
+  programs.zsh.sessionVariables = {
+    "ZSH_WAKATIME_BIN" = "${pkgs.wakatime}/bin/wakatime-cli";
+  };
 }
