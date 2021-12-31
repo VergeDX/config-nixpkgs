2d823391a7662d86e8f238a290d2f3172403ee9f
Vanilla <osu_Vanilla@126.com>
2021-12-31 21:02:32 +0800

    [-] raspi/linux_rpi4.nix: Remove self packaged linux_rpi4 kernel.

--- raspi/boot.nix
+++ raspi/boot.nix
@@ -1,10 +1,7 @@
 { pkgs, lib, ... }:
-let linux_rpi4 = pkgs.callPackage ./linux_rpi4.nix { };
-in
 {
   boot.initrd.availableKernelModules = lib.mkForce [ ];
-  # https://nixos.wiki/wiki/Linux_kernel#Pinning_a_kernel_version
-  boot.kernelPackages = pkgs.linuxPackagesFor linux_rpi4;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 
   boot.loader.generic-extlinux-compatible.enable = true;
   boot.loader.generic-extlinux-compatible.configurationLimit = 13;
