74ae55b3466e89f74cbf587aab37ac40456a1359
Vanilla <osu_Vanilla@126.com>
2021-12-31 16:09:01 +0800

    [+] packages/python3/opencv_python.nix.

---
+++ packages/python3/opencv_python.nix
@@ -0,0 +1,20 @@
+{ python3Packages, fetchPypi, ... }:
+python3Packages.buildPythonPackage rec {
+  pname = "opencv_python";
+  version = "4.5.5.62";
+  format = "wheel";
+
+  src = fetchPypi {
+    inherit pname version format;
+    hash = "sha256-EwzHXVaymqPF3otqxDgkLdJXS6bqqLzN/9z9a3hjL38=";
+
+    dist = "cp36";
+    python = "cp36";
+
+    abi = "abi3";
+    platform = "manylinux_2_17_x86_64.manylinux2014_x86_64";
+  };
+
+  propagatedBuildInputs = with python3Packages;
+    [ numpy ];
+}
