6b4d68955d465e53913fb3176d99e1c5d2309472
Vanilla <osu_Vanilla@126.com>
2021-12-31 23:30:47 +0800

    [+] raspi/networking/wireless.nix: 5522is5522.

--- raspi/networking/wireless.nix
+++ raspi/networking/wireless.nix
@@ -5,6 +5,7 @@
   networking.wireless.networks = {
     "@Ruijie-s02C6" = { psk = "Jxustnc001"; priority = 0; };
     "Redmi K30" = { psk = "aaf0bc20ba13"; priority = 1; };
+    "5522is5522" = { psk = "asdfghjkl"; priority = 0; };
   };
 
   # https://github.com/NixOS/nixpkgs/issues/101963#issuecomment-749443985
