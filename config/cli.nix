36672326925a9bb1c5110cfd66739e27f27d8799
Vanilla <osu_Vanilla@126.com>
2022-01-03 13:25:59 +0800

    [+] config/cli.nix: += ffmpeg, work with m4s.

--- config/cli.nix
+++ config/cli.nix
@@ -92,7 +92,8 @@ let nix-user-chroot = pkgs.callPackage ../packages/rust/nix-user-chroot.nix { };
     pkgs.rpm
     pkgs.bc
   ] ++ [ pkgs.nixos-generators ]
-  ++ [ sg_cli nix-user-chroot ];
+  ++ [ sg_cli nix-user-chroot ]
+  ++ [ pkgs.ffmpeg ];
 
   # https://nix-community.github.io/home-manager/options.html
   programs.exa.enable = true;
