59d6cfd20ea3c2bcaad9d4ca4f0c651d7ef9198a
Vanilla <osu_Vanilla@126.com>
2022-01-04 16:13:53 +0800

    [-] local/hardware/bluetooth.nix: Remove bluetoothd systemd override.

--- local/hardware/bluetooth.nix
+++ local/hardware/bluetooth.nix
@@ -15,11 +15,4 @@
 
   nixpkgs.config.allowUnfree = true;
   hardware.enableAllFirmware = true;
-
-  # https://raspberrypi.stackexchange.com/questions/40839/sap-error-on-bluetooth-service-status
-  # https://github.com/NixOS/nixpkgs/issues/63703
-  systemd.services.bluetooth.serviceConfig.ExecStart = [
-    ""
-    "${pkgs.bluez}/libexec/bluetooth/bluetoothd --noplugin=sap"
-  ];
 }
