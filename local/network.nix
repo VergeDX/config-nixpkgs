e9500bd1ae82883b9b318a3c05703ee90e769ea7
Vanilla <osu_Vanilla@126.com>
2022-01-07 10:41:28 +0800

    [+] local/network.nix: Enable ports for KDE Connect.

--- local/network.nix
+++ local/network.nix
@@ -11,6 +11,10 @@
   networking.firewall.allowedTCPPorts = [ 8082 ];
   networking.firewall.allowedUDPPorts = [ 5353 ];
 
+  # https://userbase.kde.org/KDEConnect#I_have_two_devices_running_KDE_Connect_on_the_same_network.2C_but_they_can.27t_see_each_other
+  networking.firewall.allowedUDPPortRanges = [{ from = 1714; to = 1764; }];
+  networking.firewall.allowedTCPPortRanges = [{ from = 1714; to = 1764; }];
+
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
