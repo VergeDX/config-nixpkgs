f5deb4f9231a185bf19ed0aed0455e06f8bc5e44
Vanilla <osu_Vanilla@126.com>
2022-01-11 11:34:30 +0800

    [M] packages/resources/gitalias.nix: Update hash.

--- packages/resources/gitalias.nix
+++ packages/resources/gitalias.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation rec {
   # https://github.com/GitAlias/gitalias#install
   src = fetchurl {
     url = "https://raw.githubusercontent.com/GitAlias/${name}/master/${fileName}";
-    sha256 = "sha256-Ov333cW2PbvX5fp1KAHfgXR4SCwjoolHGCHBf0xskkE=";
+    sha256 = "sha256-sKHqFo1legTImVxNhykmCDY24pUi1WWpUVgnUN2ujqk=";
   };
 
   unpackPhase = "echo";
