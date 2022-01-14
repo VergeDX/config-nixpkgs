7dc9fda37a5c715289d3ee798ca76c02bbf795ac
Vanilla <osu_Vanilla@126.com>
2022-01-14 15:04:56 +0800

    [+] local/nix/binaryCaches.nix: Add kdna.cachix.org.

--- local/nix/binaryCaches.nix
+++ local/nix/binaryCaches.nix
@@ -11,6 +11,7 @@
     "https://ilya-fedin.cachix.org"
     "https://across.cachix.org"
 
+    "https://kdna.cachix.org/"
     "https://cache.nixos.org/"
   ];
 
@@ -20,5 +21,8 @@
     "linyinfeng.cachix.org-1:sPYQXcNrnCf7Vr7T0YmjXz5dMZ7aOKG3EqLja0xr9MM="
     "ilya-fedin.cachix.org-1:QveU24a5ePPMh82mAFSxLk1P+w97pRxqe9rh+MJqlag="
     "across.cachix.org-1:gbHoNiu6UQBGwgEF+zAA00ovBCn5ObAER4KPqGQVI9A="
+
+    "kdna.cachix.org-1:xAkLlelk3u0frMsevzcDLsjGe6ifqXx8q+OrPkbmetI="
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
   ];
 }
