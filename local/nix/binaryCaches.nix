a083b1bfa8adf3128f11f3ee1b416cfb0383368d
Vanilla <osu_Vanilla@126.com>
2022-01-15 22:01:46 +0800

    [-] local/nix/binaryCaches.nix: Remove across cache after remove package.

--- local/nix/binaryCaches.nix
+++ local/nix/binaryCaches.nix
@@ -9,7 +9,6 @@
     "https://nix-community.cachix.org"
     "https://linyinfeng.cachix.org"
     "https://ilya-fedin.cachix.org"
-    "https://across.cachix.org"
 
     "https://kdna.cachix.org/"
     "https://cache.nixos.org/"
@@ -20,7 +19,6 @@
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "linyinfeng.cachix.org-1:sPYQXcNrnCf7Vr7T0YmjXz5dMZ7aOKG3EqLja0xr9MM="
     "ilya-fedin.cachix.org-1:QveU24a5ePPMh82mAFSxLk1P+w97pRxqe9rh+MJqlag="
-    "across.cachix.org-1:gbHoNiu6UQBGwgEF+zAA00ovBCn5ObAER4KPqGQVI9A="
 
     "kdna.cachix.org-1:xAkLlelk3u0frMsevzcDLsjGe6ifqXx8q+OrPkbmetI="
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
