e2846add195a42200d8dbe9f2d3879d08300812a
Vanilla <osu_Vanilla@126.com>
2022-01-21 00:26:21 +0800

    [+] local/services/dnscrypt-proxy2.nix: Add public DoH server from @MidAutumnHotaru.

--- local/services/dnscrypt-proxy2.nix
+++ local/services/dnscrypt-proxy2.nix
@@ -7,7 +7,13 @@
   # https://nixos.wiki/wiki/Encrypted_DNS#dnscrypt-proxy2
   services.dnscrypt-proxy2.enable = true;
   services.dnscrypt-proxy2.settings = {
+    # https://github.com/DNSCrypt/dnscrypt-proxy/issues/1301
+    # https://dnscrypt.info/stamps/ | https://owo.418.im/teapot
+    static."@MidAutumnHotaru" = {
+      "stamp" = "sdns://AgcAAAAAAAAAAAAKb3dvLjQxOC5pbQcvdGVhcG90";
+    };
+
     # https://download.dnscrypt.info/resolvers-list/v2/public-resolvers.md
-    server_names = [ "alidns-doh" "dnspod-doh" ];
+    server_names = [ "@MidAutumnHotaru" ] ++ [ "alidns-doh" "dnspod-doh" ];
   };
 }
