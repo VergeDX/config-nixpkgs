e2da42c124923a0846ca4f35e9477fce6d6d1969
Vanilla <osu_Vanilla@126.com>
2022-01-21 14:38:22 +0800

    [-] raspi/openssh.nix: Remove service fail2ban.

--- raspi/openssh.nix
+++ raspi/openssh.nix
@@ -6,7 +6,4 @@
   # https://github.com/serokell/deploy-rs/issues/25#issuecomment-740067529
   users.users."root".openssh.authorizedKeys.keys =
     [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPVpgJtK1tTpURq6XH3xibH+6VdU8/2igxpQs4aAkaoy" ];
-
-  services.fail2ban = { enable = true; ignoreIP = [ "192.168.110.182" ]; };
-  services.fail2ban.bantime-increment.enable = true;
 }
