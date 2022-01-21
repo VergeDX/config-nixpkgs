ca895712ee6c2008fced3625db325ffd51cab282
Vanilla <osu_Vanilla@126.com>
2022-01-21 14:11:16 +0800

    [+] raspi/networking/wireless.nix: Add Home wireless network Vanilla.

--- raspi/networking/wireless.nix
+++ raspi/networking/wireless.nix
@@ -6,6 +6,7 @@
     "@Ruijie-s02C6" = { psk = "Jxustnc001"; priority = 0; };
     "Redmi K30" = { psk = "aaf0bc20ba13"; priority = 1; };
     "5522is5522" = { psk = "asdfghjkl"; priority = 0; };
+    "Vanilla" = { psk = "@)))!@@%"; priority = 0; };
   };
 
   # https://github.com/NixOS/nixpkgs/issues/101963#issuecomment-749443985
