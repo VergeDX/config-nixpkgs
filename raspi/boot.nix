a7a94c58cfeb7bc1ad190ec5529722a5d68b74a2
Vanilla <osu_Vanilla@126.com>
2022-01-21 18:56:17 +0800

    [M] raspi/boot.nix: zen kernel -> rpi4.

--- raspi/boot.nix
+++ raspi/boot.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, ... }:
 {
   boot.initrd.availableKernelModules = lib.mkForce [ ];
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.kernelPackages = pkgs.linuxPackages_rpi4;
 
   boot.loader.generic-extlinux-compatible.enable = true;
   boot.loader.generic-extlinux-compatible.configurationLimit = 13;
