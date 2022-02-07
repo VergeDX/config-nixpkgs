a61aac67d07d0fc89abcc1718fae0d8a4f2691e3
Vanilla <osu_Vanilla@126.com>
2022-02-07 20:30:15 +0800

    fix[kdna]: Fix patched Kotatogram, add single quote.

--- packages/patches/kotatogram-desktop-no-ad.nix
+++ packages/patches/kotatogram-desktop-no-ad.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }:
 let dsm = "Telegram/SourceFiles/data/data_sponsored_messages.cpp"; in
 pkgs.nur.repos.ilya-fedin.kotatogram-desktop.overrideAttrs (old: {
-  patchPhase = "sed -i s/history->isChannel()/0/g ${dsm}";
+  patchPhase = "sed -i 's/history->isChannel()/0/g' ${dsm}";
 })
